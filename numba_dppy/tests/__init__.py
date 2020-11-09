965e13d35cd013f6dcbd5bb39baf57383d12b702
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-11-10 00:29:43 +0300

    Move numba/dppl_config.py to numba_dppy/config.py
    dppl_config.py contains flag `dppl_present` which indicate that `dpCtl`
    and devices are available.
    As `numba-dppy` will depend on `dpCtl` then the check is not necessary.
    Also checking for available platform and device could be done in
    `numba-dppy`. It also can raise and exception on import as before.
    
    `dppl_present` also renamed to `dppy_present`.

--- numba_dppy/tests/__init__.py
+++ numba_dppy/tests/__init__.py
@@ -3,14 +3,14 @@ from numba.testing import load_testsuite
 from os.path import dirname, join
 
 
-import numba.dppl_config as dppl_config
+import numba_dppy.config as dppy_config
 
 def load_tests(loader, tests, pattern):
 
     suite = SerialSuite()
     this_dir = dirname(__file__)
 
-    if dppl_config.dppl_present:
+    if dppy_config.dppy_present:
         suite.addTests(load_testsuite(loader, join(this_dir, 'dppl')))
     else:
         print("skipped DPPL tests")
