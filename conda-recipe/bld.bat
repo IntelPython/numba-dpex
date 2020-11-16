b7f8c3ca02068ae2e03baa395d70f0884b7d4b25
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-11-16 07:08:12 -0600

    Add compilation of SPIRV code

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -11,6 +11,6 @@ echo on
 
 set "CC=clang.exe"
 
-%CC% -flto -target spir64-unknown-unknown -c -x cl -emit-llvm -cl-std=CL2.0 -Xclang -finclude-default-header numba/dppl/ocl/atomics/atomic_ops.cl -o numba/dppl/ocl/atomics/atomic_ops.bc
-llvm-spirv -o numba/dppl/ocl/atomics/atomic_ops.spir numba/dppl/ocl/atomics/atomic_ops.bc
-xcopy numba\dppl\ocl\atomics\atomic_ops.spir %SP_DIR%\numba\dppl\ocl\atomics /E /Y
+%CC% -flto -target spir64-unknown-unknown -c -x cl -emit-llvm -cl-std=CL2.0 -Xclang -finclude-default-header numba_dppy/ocl/atomics/atomic_ops.cl -o numba_dppy/ocl/atomics/atomic_ops.bc
+llvm-spirv -o numba_dppy/ocl/atomics/atomic_ops.spir numba_dppy/ocl/atomics/atomic_ops.bc
+xcopy numba_dppy\ocl\atomics\atomic_ops.spir %SP_DIR%\numba_dppy\ocl\atomics /E /Y
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -10,6 +10,6 @@ else
     echo "DPCPP is needed to build OpenCL kernel. Abort!"
 fi
 
-$CC -flto -target spir64-unknown-unknown -c -x cl -emit-llvm -cl-std=CL2.0 -Xclang -finclude-default-header numba/dppl/ocl/atomics/atomic_ops.cl -o numba/dppl/ocl/atomics/atomic_ops.bc
-llvm-spirv -o numba/dppl/ocl/atomics/atomic_ops.spir numba/dppl/ocl/atomics/atomic_ops.bc
-cp numba/dppl/ocl/atomics/atomic_ops.spir ${SP_DIR}/numba/dppl/ocl/atomics/
+${CC} -flto -target spir64-unknown-unknown -c -x cl -emit-llvm -cl-std=CL2.0 -Xclang -finclude-default-header numba_dppy/ocl/atomics/atomic_ops.cl -o numba_dppy/ocl/atomics/atomic_ops.bc
+llvm-spirv -o numba_dppy/ocl/atomics/atomic_ops.spir numba_dppy/ocl/atomics/atomic_ops.bc
+cp numba_dppy/ocl/atomics/atomic_ops.spir ${SP_DIR}/numba_dppy/ocl/atomics/
