e146f730e4c4ac894d790d142f8547a373d6e39b
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-11-20 15:16:36 +0300

    Update run_test.sh

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # For activating OpenCL CPU
 source ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
 source ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
+
+set -x
 
 python -m numba.runtests -b -v -m -- numba_dppy.tests
 
