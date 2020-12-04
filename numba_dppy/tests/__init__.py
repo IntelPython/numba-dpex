bba251e88ea6a756cd2609615083031fa09dbdbd
etotmeni <elena.totmenina@intel.com>
2020-12-04 05:37:30 -0600

    wip

--- numba_dppy/tests/__init__.py
+++ numba_dppy/tests/__init__.py
@@ -2,15 +2,19 @@ from numba.testing import SerialSuite
 from numba.testing import load_testsuite
 from os.path import dirname, join
 
-
+import numba_dppy
 import numba_dppy.config as dppy_config
+
+# from numba_dppy.tests.dppy import *
 
 def load_tests(loader, tests, pattern):
 
     suite = SerialSuite()
+    # tests = loader.loadTestsFromModule(numba_dppy.tests)
     this_dir = dirname(__file__)
 
     if dppy_config.dppy_present:
+        # suite.addTests(tests)
         suite.addTests(load_testsuite(loader, join(this_dir, 'dppy')))
     else:
         print("skipped DPPY tests")
