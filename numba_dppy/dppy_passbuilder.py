0fc597a3aa83e928d26c1b6412a47c8d02b7bdca
reazul.hoque <reazul.hoque@intel.com>
2020-12-08 10:43:11 -0600

    Added comments

--- numba_dppy/dppy_passbuilder.py
+++ numba_dppy/dppy_passbuilder.py
@@ -2,32 +2,49 @@ from __future__ import print_function, division, absolute_import
 
 from numba.core.compiler_machinery import PassManager
 
-from numba.core.untyped_passes import (ExtractByteCode, TranslateByteCode, FixupArgs,
-                                  IRProcessing, DeadBranchPrune,
-                                  RewriteSemanticConstants, InlineClosureLikes,
-                                  GenericRewrites, WithLifting,
-                                  InlineInlinables, FindLiterallyCalls,
-                                  MakeFunctionToJitFunction,
-                                  CanonicalizeLoopExit, CanonicalizeLoopEntry,
-                                  ReconstructSSA,
-                                  LiteralUnroll)
+from numba.core.untyped_passes import (
+    ExtractByteCode,
+    TranslateByteCode,
+    FixupArgs,
+    IRProcessing,
+    DeadBranchPrune,
+    RewriteSemanticConstants,
+    InlineClosureLikes,
+    GenericRewrites,
+    WithLifting,
+    InlineInlinables,
+    FindLiterallyCalls,
+    MakeFunctionToJitFunction,
+    CanonicalizeLoopExit,
+    CanonicalizeLoopEntry,
+    ReconstructSSA,
+    LiteralUnroll,
+)
 
-from numba.core.typed_passes import (NopythonTypeInference, AnnotateTypes,
-                                NopythonRewrites, PreParforPass, ParforPass,
-                                DumpParforDiagnostics, IRLegalization,
-                                InlineOverloads, PreLowerStripPhis)
+from numba.core.typed_passes import (
+    NopythonTypeInference,
+    AnnotateTypes,
+    NopythonRewrites,
+    PreParforPass,
+    ParforPass,
+    DumpParforDiagnostics,
+    IRLegalization,
+    InlineOverloads,
+    PreLowerStripPhis,
+)
 
 from .dppy_passes import (
-        DPPYConstantSizeStaticLocalMemoryPass,
-        DPPYPreParforPass,
-        DPPYParforPass,
-        SpirvFriendlyLowering,
-        DPPYAddNumpyOverloadPass,
-        DPPYAddNumpyRemoveOverloadPass,
-        DPPYNoPythonBackend
-        )
+    DPPYConstantSizeStaticLocalMemoryPass,
+    DPPYPreParforPass,
+    DPPYParforPass,
+    SpirvFriendlyLowering,
+    DPPYAddNumpyOverloadPass,
+    DPPYAddNumpyRemoveOverloadPass,
+    DPPYNoPythonBackend,
+)
 
-from .rename_numpy_functions_pass import (DPPYRewriteOverloadedFunctions)
+from .rename_numpy_functions_pass import DPPYRewriteOverloadedFunctions
+
 
 class DPPYPassBuilder(object):
     """
@@ -47,16 +64,24 @@ class DPPYPassBuilder(object):
         pm.add_pass(WithLifting, "Handle with contexts")
 
         # this pass rewrite name of functions
-        pm.add_pass(DPPYRewriteOverloadedFunctions, "dppy rewrite name of functions")
+        pm.add_pass(
+            DPPYRewriteOverloadedFunctions,
+            "Rewrite name of Numpy functions to overload already overloaded function",
+        )
 
         # this pass adds required logic to overload default implementation of
         # Numpy functions
-        pm.add_pass(DPPYAddNumpyOverloadPass, "dppy add typing template for Numpy functions")
+        pm.add_pass(
+            DPPYAddNumpyOverloadPass, "dppy add typing template for Numpy functions"
+        )
 
         # Add pass to ensure when users are allocating static
         # constant memory the size is a constant and can not
         # come from a closure variable
-        pm.add_pass(DPPYConstantSizeStaticLocalMemoryPass, "dppy constant size for static local memory")
+        pm.add_pass(
+            DPPYConstantSizeStaticLocalMemoryPass,
+            "dppy constant size for static local memory",
+        )
 
         # pre typing
         if not state.flags.no_rewrites:
@@ -64,11 +89,11 @@ class DPPYPassBuilder(object):
             pm.add_pass(DeadBranchPrune, "dead branch pruning")
             pm.add_pass(GenericRewrites, "nopython rewrites")
 
-        pm.add_pass(InlineClosureLikes,
-                    "inline calls to locally defined closures")
+        pm.add_pass(InlineClosureLikes, "inline calls to locally defined closures")
         # convert any remaining closures into functions
-        pm.add_pass(MakeFunctionToJitFunction,
-                    "convert make_function into JIT functions")
+        pm.add_pass(
+            MakeFunctionToJitFunction, "convert make_function into JIT functions"
+        )
         # inline functions that have been determined as inlinable and rerun
         # branch pruning, this needs to be run after closures are inlined as
         # the IR repr of a closure masks call sites if an inlinable is called
@@ -92,10 +117,8 @@ class DPPYPassBuilder(object):
         # optimisation
         pm.add_pass(InlineOverloads, "inline overloaded functions")
 
-
-
     @staticmethod
-    def define_nopython_pipeline(state, name='dppy_nopython'):
+    def define_nopython_pipeline(state, name="dppy_nopython"):
         """Returns an nopython mode pipeline based PassManager
         """
         pm = PassManager(name)
@@ -113,6 +136,9 @@ class DPPYPassBuilder(object):
         # lower
         pm.add_pass(SpirvFriendlyLowering, "SPIRV-friendly lowering pass")
         pm.add_pass(DPPYNoPythonBackend, "nopython mode backend")
-        pm.add_pass(DPPYAddNumpyRemoveOverloadPass, "dppy remove typing template for Numpy functions")
+        pm.add_pass(
+            DPPYAddNumpyRemoveOverloadPass,
+            "dppy remove typing template for Numpy functions",
+        )
         pm.finalize()
         return pm
