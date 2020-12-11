9800d9a49bc629edfd50e048e3db8da256c957c7
etotmeni <elena.totmenina@intel.com>
2020-12-11 09:54:32 -0600

    Rewrite tests

--- numba_dppy/tests/test_offload_diagnostics.py
+++ numba_dppy/tests/test_offload_diagnostics.py
@@ -8,26 +8,19 @@ from numba.tests.support import captured_stdout
 import dpctl
 
 
-def prange_func():
-    n = 10
-    a = np.ones((n), dtype=np.float64)
-    b = np.ones((n), dtype=np.float64)
-    c = np.ones((n), dtype=np.float64)
-    for i in prange(n//2):
-        a[i] = b[i] + c[i]
-
-    return a
-
-
-@dppy.kernel
-def parallel_sum(a, b, c):
-    i = dppy.get_global_id(0)
-    c[i] = a[i] + b[i]
-
-
 @unittest.skipUnless(dpctl.has_gpu_queues(), "test only on GPU system")
 class TestOffloadDiagnostics(unittest.TestCase):
     def test_parfor(self):
+        def prange_func():
+            n = 10
+            a = np.ones((n), dtype=np.float64)
+            b = np.ones((n), dtype=np.float64)
+            c = np.ones((n), dtype=np.float64)
+            for i in prange(n//2):
+                a[i] = b[i] + c[i]
+
+            return a
+
         with dpctl.device_context("opencl:gpu"):
             dppy_config.OFFLOAD_DIAGNOSTICS = 1
             jitted = njit(parallel=True)(prange_func)
@@ -40,6 +33,11 @@ class TestOffloadDiagnostics(unittest.TestCase):
             self.assertTrue("Device -" in got.getvalue())
 
     def test_kernel(self):
+        @dppy.kernel
+        def parallel_sum(a, b, c):
+            i = dppy.get_global_id(0)
+            c[i] = a[i] + b[i]
+
         global_size = 10
         N = global_size
 
