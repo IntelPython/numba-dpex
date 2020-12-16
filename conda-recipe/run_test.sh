b00afc651c3dbb42dd80e8b95dd3f493a4eeb194
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-12-16 16:53:29 -0600

    fix

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -8,6 +8,7 @@ source ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
 
 set -x
 
-python -m numba.runtests -b -v -m -- numba_dppy.tests
+export NUMBA_DEBUG=1
+python -m numba.runtests -b -v -m -- numba_dppy.tests.test_usmarray.TestUsmArray.test_numba_usmarray_as_ndarray
 
 exit 0
