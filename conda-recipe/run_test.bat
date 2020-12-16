0e71fa7b8de25ae1aefc8c3f5d670b8bdca2d087
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2020-12-16 16:17:35 -0600

    one test in debug

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -3,7 +3,9 @@ call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
 
 @echo on
 
-python -m numba.runtests -b -v -m -- numba_dppy.tests
+export NUMBA_DEBUG=1
+
+python -m numba.runtests -b -v -m -- numba_dppy.tests.test_usmarray.TestUsmArray.test_numba_usmarray_as_ndarray
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 exit /B 0
