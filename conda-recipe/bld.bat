e51d557db81c117b0066c91a2697410db7dcafcd
etotmeni <elena.totmenina@intel.com>
2020-12-17 17:18:51 +0300

    Remove errorcheck

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -5,4 +5,3 @@ REM conda uses %ERRORLEVEL% but FPGA scripts can set it. So it should be reseted
 set ERRORLEVEL=
 
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
