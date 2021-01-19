dc0eea33fc730a8b3abd39b91445e2ecb7df7408
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-01-19 06:55:30 -0600

    recommonmark

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = '0.12.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
