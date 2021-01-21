e4a5a5cc6e094bb71393de8c7b060e76568a5a78
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-01-21 19:11:43 +0300

    Docs for GPU Reduction (#185)
    * Example 1
    * Example 2
    * Example 3
    * Limitations

---
+++ docs/dppy/reduction.rst
@@ -0,0 +1,71 @@
+GPU Reduction
+=============
+
+DPPY does not provide specific features for implementing reductions on GPU.
+Examples contain different approaches for calculating reductions using both
+device and host.
+
+Examples
+--------
+
+Example 1
+~~~~~~~~~
+
+This example demonstrates a sum reduction of 1d array.
+
+Full example can be found at ``numba_dppy/examples/sum_reduction.py``.
+
+In this example to sum the 1d array we invoke the Kernel multiple times.
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction.py
+   :pyobject: sum_reduction_kernel
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction.py
+   :pyobject: sum_reduce
+
+Example 2
+~~~~~~~~~
+
+Full example can be found at ``numba_dppy/examples/sum_reduction_ocl.py``.
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction_ocl.py
+   :pyobject: sum_reduction_kernel
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction_ocl.py
+   :pyobject: sum_reduce
+
+Example 3
+~~~~~~~~~
+
+Full example can be found at ``numba_dppy/examples/sum_reduction_recursive_ocl.py``
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction_recursive_ocl.py
+   :pyobject: sum_reduction_kernel
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction_recursive_ocl.py
+   :pyobject: sum_recursive_reduction
+
+.. literalinclude:: ../../numba_dppy/examples/sum_reduction_recursive_ocl.py
+   :pyobject: sum_reduce
+
+Limitations
+-----------
+
+Examples show invocation of the kernel multiple times.
+Reduction could be implemented by invoking the kernel once, but that requires
+support for local device memory and barrier, which is a work in progress.
+
+Transition from Numba CUDA
+--------------------------
+
+DPPY does not provide ``@reduce`` decorator for writing a reduction algorithm
+for DPPY GPU.
+
+See also
+--------
+
+Examples:
+
+- ``numba_dppy/examples/sum_reduction.py``
+- ``numba_dppy/examples/sum_reduction_ocl.py``
+- ``numba_dppy/examples/sum_reduction_recursive_ocl.py``
--- numba_dppy/examples/sum_reduction_ocl.py
+++ numba_dppy/examples/sum_reduction_ocl.py
@@ -1,64 +1,82 @@
-import sys
 import numpy as np
 from numba import int32
-import numba_dppy, numba_dppy as dppy
+import numba_dppy as dppy
 import math
 
 import dpctl
 
 
-def sum_reduction_device_plus_host():
-    @dppy.kernel
-    def sum_reduction_kernel(inp, partial_sums):
-        local_id = dppy.get_local_id(0)
-        global_id = dppy.get_global_id(0)
-        group_size = dppy.get_local_size(0)
-        group_id = dppy.get_group_id(0)
+@dppy.kernel
+def sum_reduction_kernel(A, partial_sums):
+    local_id = dppy.get_local_id(0)
+    global_id = dppy.get_global_id(0)
+    group_size = dppy.get_local_size(0)
+    group_id = dppy.get_group_id(0)
 
-        local_sums = dppy.local.static_alloc(64, int32)
+    local_sums = dppy.local.static_alloc(64, int32)
 
-        # Copy from global to local memory
-        local_sums[local_id] = inp[global_id]
+    # Copy from global to local memory
+    local_sums[local_id] = A[global_id]
 
-        # Loop for computing local_sums : divide workgroup into 2 parts
-        stride = group_size // 2
-        while stride > 0:
-            # Waiting for each 2x2 addition into given workgroup
-            dppy.barrier(dppy.CLK_LOCAL_MEM_FENCE)
+    # Loop for computing local_sums : divide workgroup into 2 parts
+    stride = group_size // 2
+    while stride > 0:
+        # Waiting for each 2x2 addition into given workgroup
+        dppy.barrier(dppy.CLK_LOCAL_MEM_FENCE)
 
-            # Add elements 2 by 2 between local_id and local_id + stride
-            if local_id < stride:
-                local_sums[local_id] += local_sums[local_id + stride]
+        # Add elements 2 by 2 between local_id and local_id + stride
+        if local_id < stride:
+            local_sums[local_id] += local_sums[local_id + stride]
 
-            stride >>= 1
+        stride >>= 1
 
-        if local_id == 0:
-            partial_sums[group_id] = local_sums[0]
+    if local_id == 0:
+        partial_sums[group_id] = local_sums[0]
 
-    global_size = 1024
+
+def get_context():
+    if dpctl.has_gpu_queues():
+        return "opencl:gpu"
+    elif dpctl.has_cpu_queues():
+        return "opencl:cpu"
+    else:
+        raise RuntimeError("No device found")
+
+
+def sum_reduce(A):
+    global_size = len(A)
     work_group_size = 64
     # nb_work_groups have to be even for this implementation
     nb_work_groups = global_size // work_group_size
 
-    inp = np.ones(global_size).astype(np.int32)
-    partial_sums = np.zeros(nb_work_groups).astype(np.int32)
+    partial_sums = np.zeros(nb_work_groups).astype(A.dtype)
 
-    if dpctl.has_gpu_queues():
-        with dpctl.device_context("opencl:gpu") as gpu_queue:
-            print("Running Device + Host reduction")
-            sum_reduction_kernel[global_size, work_group_size](inp, partial_sums)
-    else:
-        print("No device found")
-        exit()
+    context = get_context()
+    with dpctl.device_context(context):
+        sum_reduction_kernel[global_size, work_group_size](A, partial_sums)
 
     final_sum = 0
     # calculate the final sum in HOST
     for i in range(nb_work_groups):
         final_sum += partial_sums[i]
 
-    assert final_sum == global_size
-    print("Expected:", global_size, "--- GOT:", final_sum)
+    return final_sum
+
+
+def test_sum_reduce():
+    N = 1024
+    A = np.ones(N).astype(np.int32)
+
+    print("Running Device + Host reduction")
+
+    actual = sum_reduce(A)
+    expected = N
+
+    print("Actual:  ", actual)
+    print("Expected:", expected)
+
+    assert actual == expected
 
 
 if __name__ == "__main__":
-    sum_reduction_device_plus_host()
+    test_sum_reduce()
