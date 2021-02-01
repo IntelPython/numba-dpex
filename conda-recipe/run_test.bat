9621223849eea543245dd54c45aa48ccc3cd5330
etotmeni <elena.totmenina@intel.com>
2021-02-01 06:42:19 -0600

    win

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -3,7 +3,7 @@ call "%ONEAPI_ROOT%\compiler\latest\env\vars.bat"
 
 @echo on
 
-python -m numba.runtests -b -v -m -- numba_dppy.tests
+python -m pytest --pyargs numba_dppy.tests
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 exit /B 0
