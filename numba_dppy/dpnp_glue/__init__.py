23f207d6a40afe9928e851fbcd33e2cc2133d555
Diptorup Deb <diptorup.deb@intel.com>
2021-04-03 21:36:02 -0500

    dpNP -> dpnp

--- numba_dppy/dpnp_glue/__init__.py
+++ numba_dppy/dpnp_glue/__init__.py
@@ -18,7 +18,7 @@ def ensure_dpnp(name):
         # import dpnp
         from . import dpnp_fptr_interface as dpnp_glue
     except ImportError:
-        raise ImportError("dpNP is needed to call np.%s" % name)
+        raise ImportError("dpnp is needed to call np.%s" % name)
 
 
 DEBUG = None
