bc0cc5a106a14c15ee77652fa8e9db562ba1cc32
akharche <angelina.kharchevnikova@intel.com>
2021-04-05 22:25:24 +0300

    Rewrite to pytest

--- numba_dppy/tests/test_debug.py
+++ numba_dppy/tests/test_debug.py
@@ -14,68 +14,80 @@
 # limitations under the License.
 
 import re
-import unittest
+import pytest
 
 import numba_dppy as dppy
 import dpctl
 
-from numba.tests.support import TestCase
 from numba.core import types, compiler
 from numba_dppy import compiler
+from numba_dppy.tests.skip_tests import skip_test
 
 
-@unittest.skipUnless(dpctl.has_gpu_queues(), "test only on GPU system")
-class TestDebugInfo(TestCase):
+# TODO: Add level0 and move to common place
+offload_devices = [
+    "opencl:gpu:0",
+    "opencl:cpu:0",
+]
+
+
+@pytest.fixture(params=offload_devices)
+def offload_device(request):
+    return request.param
+
+
+def get_kernel_ir(fn, sig, debug=False):
+    kernel = compiler.compile_kernel(
+        fn.sycl_queue, fn.py_func, sig, None, debug=debug
+    )
+    return kernel.assembly
+
+
+def make_check(fn, sig, expect):
     """
-    These tests only check the compiled assembly for debuginfo.
+    Check the compiled assembly for debuginfo.
+    """
+    ir = get_kernel_ir(fn, sig=sig, debug=expect)
+
+    # Checking whether debug symbols have been emmited to IR
+    m = re.search(r"!dbg", ir, re.I)
+    got = m is not None
+    assert expect == got
+
+
+def test_debuginfo_in_ir(offload_device):
+    """
+    Check debug info is emitting to IR if debug parameter is set to True
     """
 
-    def get_kernel_asm(self, fn, sig, debug=False):
-        kernel = compiler.compile_kernel(
-            fn.sycl_queue, fn.py_func, sig, None, debug=debug
-        )
-        return kernel.assembly
+    if skip_test(offload_device):
+        pytest.skip()
 
-    def make_check(self, fn, sig, expect):
-        asm = self.get_kernel_asm(fn, sig=sig, debug=expect)
+    debug_expect = True
 
-        # Checking whether debug symbols have been emmited to IR
-        m = re.search(r"!dbg", asm, re.I)
-        got = m is not None
-        self.assertEqual(expect, got, msg="debug info not found in kernel:\n%s" % fn)
+    @dppy.kernel
+    def foo(x):
+        return x
 
-    def test_debuginfo_in_asm(self):
-        """
-        Check debug info is emitting to IR if debug parameter is set to True
-        """
-
-        debug_expect = True
-
-        @dppy.kernel
-        def foo(x):
-            return x
-
-        if dpctl.has_gpu_queues():
-            with dpctl.device_context("opencl:gpu") as gpu_queue:
-                sig = (types.int32,)
-                self.make_check(foo, sig, debug_expect)
-
-    def test_debuginfo_not_in_asm(self):
-        """
-        Check debug info is not emitting to IR if debug parameter is set to True
-        """
-
-        debug_expect = False
-
-        @dppy.kernel
-        def foo(x):
-            return x
-
-        if dpctl.has_gpu_queues():
-            with dpctl.device_context("opencl:gpu") as gpu_queue:
-                sig = (types.int32,)
-                self.make_check(foo, sig, debug_expect)
+    with dpctl.device_context(offload_device):
+        sig = (types.int32,)
+        make_check(foo, sig, debug_expect)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_debuginfo_not_in_ir(offload_device):
+    """
+    Check debug info is not emitting to IR if debug parameter is set to True
+    """
+
+    if skip_test(offload_device):
+        pytest.skip()
+
+    debug_expect = False
+
+    @dppy.kernel
+    def foo(x):
+        return x
+
+    with dpctl.device_context(offload_device):
+        sig = (types.int32,)
+        make_check(foo, sig, debug_expect)
