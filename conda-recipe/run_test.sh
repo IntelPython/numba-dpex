2203dd8e17ab09247c73435b01314dcec594d110
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2021-04-08 04:35:12 -0500

    Use pytest-cov

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -8,6 +8,6 @@ source ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
 
 set -x
 
-coverage run -m --source=numba_dppy --branch --omit=*/numba_dppy/tests/*,*/numba_dppy/_version.py pytest -q -ra --disable-warnings --pyargs numba_dppy -vv && coverage report -m
+pytest -q -ra --disable-warnings --cov numba_dppy --cov-report term-missing --pyargs numba_dppy -vv
 
 exit 0
