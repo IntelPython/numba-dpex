1c344c66692a733e8052e431a0d54b4b507c971d
Rubtsowa <mariyarubtsowa@yandex.ru>
2021-05-26 11:51:31 +0300

    change test

--- numba_dppy/tests/njit_tests/dpnp/test_numpy_indexing.py
+++ numba_dppy/tests/njit_tests/dpnp/test_numpy_indexing.py
@@ -25,6 +25,18 @@ from numba_dppy.testing import dpnp_debug
 from .dpnp_skip_test import dpnp_skip_test as skip_test
 
 
+list_of_filter_strs = [
+    "opencl:gpu:0",
+    "level_zero:gpu:0",
+    "opencl:cpu:0",
+]
+
+
+@pytest.fixture(params=list_of_filter_strs)
+def filter_str(request):
+    return request.param
+
+
 @pytest.mark.parametrize("offset", [0, 1], ids=["0", "1"])
 @pytest.mark.parametrize(
     "array",
@@ -57,8 +69,8 @@ from .dpnp_skip_test import dpnp_skip_test as skip_test
         "[[[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]], [[[13, 14, 15], [16, 17, 18]], [[19, 20, 21], [22, 23, 24]]]]",
     ],
 )
-def test_diagonal(array, offset):
-    if skip_test("opencl:gpu:0"):
+def test_diagonal(array, offset, filter_str):
+    if skip_test(filter_str):
         pytest.skip()
 
     a = np.array(array)
@@ -67,7 +79,7 @@ def test_diagonal(array, offset):
         return np.diagonal(a, offset)
 
     f = njit(fn)
-    with dpctl.device_context("opencl:gpu:0"), dpnp_debug():
+    with dpctl.device_context(filter_str), dpnp_debug():
         actual = f(a, offset)
 
     expected = fn(a, offset)
