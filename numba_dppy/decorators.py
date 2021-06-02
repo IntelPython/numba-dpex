04529af012d149fc92046a6a6d53c4f1bc6c9a76
Diptorup Deb <diptorup.deb@intel.com>
2021-06-02 10:47:08 -0500

    Remove imports from future. (#417)
    Numba now only supports Python 3, we do not need the imports from `__future__` any more in our sources as well.

--- numba_dppy/decorators.py
+++ numba_dppy/decorators.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function, absolute_import, division
 from numba.core import sigutils, types
 from .compiler import (
     compile_kernel,
