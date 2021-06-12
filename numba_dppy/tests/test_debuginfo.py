8d47a243e1bca59f188cf9dc9b8cbf12368dd8c7
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-06-12 05:01:04 -0500

    Update test_debug_flag_generates_ir_with_debuginfo_for_func

--- numba_dppy/tests/test_debuginfo.py
+++ numba_dppy/tests/test_debuginfo.py
@@ -128,42 +128,37 @@ def test_debug_kernel_local_vars_in_ir():
         assert tag in kernel_ir
 
 
-def test_debug_flag_generates_ir_with_debuginfo_for_func(offload_device):
+def test_debug_flag_generates_ir_with_debuginfo_for_func(debug_option):
     """
     Check debug info is emitting to IR if debug parameter is set to True
     """
 
-    if skip_test(offload_device):
-        pytest.skip()
-
-    if offload_device in "level_zero:gpu:0":
-        pytest.xfail("Failing compilation: SyclProgramCompilationError")
-
-    @dppy.func(debug=True)
+    @dppy.func(debug=debug_option)
     def func_sum(a, b):
         result = a + b
         return result
 
-    @dppy.kernel(debug=True)
+    @dppy.kernel(debug=debug_option)
     def data_parallel_sum(a, b, c):
         i = dppy.get_global_id(0)
         c[i] = func_sum(a[i], b[i])
 
-    ir_tag_func_sum = r'\!DISubprogram\(name: ".*func_sum"'
-    ir_tag_data_parallel_sum = r'\!DISubprogram\(name: ".*data_parallel_sum"'
-    ir_tags = (ir_tag_func_sum, ir_tag_data_parallel_sum)
+    ir_tags = [
+        r'\!DISubprogram\(name: ".*func_sum"',
+        r'\!DISubprogram\(name: ".*data_parallel_sum"',
+    ]
 
-    with dpctl.device_context(offload_device) as sycl_queue:
-        sig = (
-            types.float32[:],
-            types.float32[:],
-            types.float32[:],
-        )
-        kernel_ir = get_kernel_ir(sycl_queue, data_parallel_sum, sig, debug=True)
+    sycl_queue = dpctl.get_current_queue()
+    sig = (
+        types.float32[:],
+        types.float32[:],
+        types.float32[:],
+    )
+
+    kernel_ir = get_kernel_ir(sycl_queue, data_parallel_sum, sig, debug=debug_option)
 
     for tag in ir_tags:
-        got = make_check(kernel_ir, tag)
-        assert got == True
+        assert debug_option == make_check(kernel_ir, tag)
 
 
 def test_env_var_generates_ir_with_debuginfo_for_func(offload_device):
