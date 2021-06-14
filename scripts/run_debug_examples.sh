620e4eeec6feba7f17aaf29d223f79c14cef3aea
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-06-14 07:54:38 -0500

    Add scripts for testing debug examples

---
+++ scripts/run_debug_examples.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+check() {
+  echo "Run $1 ..."
+  (cd numba_dppy/examples/debug && NUMBA_DPPY_DEBUGINFO=1 gdb-oneapi -q -command $1 python) | grep Done
+  # python $1 | grep Done
+}
+
+run_checks() {
+  check commands/function_breakpoint
+  # check commands/local_variables
+  check commands/stepping
+}
+
+run_with_device() {
+  echo "Run with SYCL_DEVICE_FILTER=$1 ..."
+  SYCL_DEVICE_FILTER=$1 run_checks
+}
+
+run_with_device level_zero:gpu:0
+run_with_device opencl:gpu:0
+# run_with_device opencl:cpu:0
+
+echo Done
