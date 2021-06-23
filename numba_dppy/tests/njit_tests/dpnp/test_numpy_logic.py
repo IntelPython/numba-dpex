f319823e5d094cc31a3f80f5c2e27047c6cdb44a
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-06-23 10:28:15 -0500

    Fix merge with main

--- numba_dppy/tests/njit_tests/dpnp/test_numpy_logic.py
+++ numba_dppy/tests/njit_tests/dpnp/test_numpy_logic.py
@@ -20,7 +20,7 @@ import dpctl
 import numpy as np
 from numba import njit
 import pytest
-from numba_dppy.testing import dpnp_debug
+from numba_dppy.tests._helper import dpnp_debug
 from .dpnp_skip_test import dpnp_skip_test as skip_test
 
 
