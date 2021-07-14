a60c6eb1a3ad78e111cd1c16d2c59662276ca039
reazul.hoque <reazul.hoque@intel.com>
2021-07-14 10:40:37 -0500

    Update vectorize example

--- numba_dppy/examples/vectorize.py
+++ numba_dppy/examples/vectorize.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import numpy as np
-from numba import vectorize
+from numba import vectorize, float64
 import dpctl
 
 
@@ -34,7 +34,7 @@ def get_device():
     return device
 
 
-def main():
+def test_njit():
     N = 10
     dtype = np.float64
 
@@ -55,5 +55,21 @@ def main():
     print("Done...")
 
 
+@vectorize([float64(float64, float64)], target="dppy")
+def vector_add(a, b):
+    return a + b
+
+
+def test_vectorize():
+    A = np.arange(10, dtype=np.float64).reshape((5, 2))
+    B = np.arange(10, dtype=np.float64).reshape((5, 2))
+
+    with dpctl.device_context("opencl:gpu:0"):
+        C = vector_add(A, B)
+
+    print(C)
+
+
 if __name__ == "__main__":
-    main()
+    test_njit()
+    test_vectorize()
