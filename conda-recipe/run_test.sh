999f902760d809f651d886115a8c0f386025da3d
reazul.hoque <reazul.hoque@intel.com>
2021-08-25 19:14:40 -0500

    Remove ONEAPI_ROOT

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 set -e
-
-# For activating OpenCL CPU
-source ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
-source ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
-
 set -x
 
 pytest -q -ra --disable-warnings --pyargs numba_dppy -vv
