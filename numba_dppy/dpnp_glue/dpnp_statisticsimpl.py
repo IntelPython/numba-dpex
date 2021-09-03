d830acd5a8aa708aa650a02a9db1f0f38fb4b91f
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-09-03 19:08:37 +0300

    Update dpnp_max_c signature

--- numba_dppy/dpnp_glue/dpnp_statisticsimpl.py
+++ numba_dppy/dpnp_glue/dpnp_statisticsimpl.py
@@ -31,11 +31,10 @@ def dpnp_amax_impl(a):
     ret_type = types.void
     """
     dpnp source:
-    https://github.com/IntelPython/dpnp/blob/0.7.0/dpnp/backend/kernels/dpnp_krnl_statistics.cpp#L147
+    https://github.com/IntelPython/dpnp/blob/e389248c709531b181be8bf33b1a270fca812a92/dpnp/backend/kernels/dpnp_krnl_statistics.cpp#L149
 
     Function declaration:
-    void dpnp_max_c(void* array1_in, void* result1, const size_t* shape,
-                    size_t ndim, const size_t* axis, size_t naxis)
+    void dpnp_max_c(void* array1_in, void* result1, const size_t result_size, const size_t* shape, size_t ndim, const size_t* axis, size_t naxis)
 
     We are using void * in case of size_t * as Numba currently does not have
     any type to represent size_t *. Since, both the types are pointers,
@@ -46,6 +45,7 @@ def dpnp_amax_impl(a):
         ret_type,
         types.voidptr,
         types.voidptr,
+        types.intp,
         types.voidptr,
         types.intp,
         types.voidptr,
@@ -67,7 +67,7 @@ def dpnp_amax_impl(a):
 
         axis, naxis = 0, 0
 
-        dpnp_func(a_usm, out_usm, a.shapeptr, a.ndim, axis, naxis)
+        dpnp_func(a_usm, out_usm, a.size * a.itemsize, a.shapeptr, a.ndim, axis, naxis)
 
         out = np.empty(1, dtype=a.dtype)
         dpctl_functions.queue_memcpy(
