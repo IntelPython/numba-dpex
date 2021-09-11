9502cb77c044930fafcb027a5743f1a885ee4bbb
Sergey Pokhodenko <pokhodenko.s.a@gmail.com>
2021-09-12 02:35:47 +0300

    Fix flake8

--- numba_dppy/driver/dpctl_capi_fn_builder.py
+++ numba_dppy/driver/dpctl_capi_fn_builder.py
@@ -18,7 +18,6 @@ declarations into an LLVM module.
 """
 
 import llvmlite.llvmpy.core as lc
-from llvmlite.ir import builder
 from numba.core import cgutils, types
 
 import numba_dppy.utils as utils
