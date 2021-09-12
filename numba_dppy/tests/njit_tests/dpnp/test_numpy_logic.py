63a0eadffd5ae6e2841c6bfa53b2e4bd16fa8f6f
Sergey Pokhodenko <pokhodenko.s.a@gmail.com>
2021-09-12 17:17:14 +0300

    Fix flake8

--- numba_dppy/tests/njit_tests/dpnp/test_numpy_logic.py
+++ numba_dppy/tests/njit_tests/dpnp/test_numpy_logic.py
@@ -16,7 +16,6 @@
 # limitations under the License.
 ################################################################################
 
-import dpctl
 import numpy as np
 import pytest
 from numba import njit
