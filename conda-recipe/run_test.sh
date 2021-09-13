bf231629dba2e7e56163e125004f5985835bdda0
Sergey Pokhodenko <pokhodenko.s.a@gmail.com>
2021-09-13 08:47:56 +0300

    Allow undefined variables in oneAPI vars.sh

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -5,8 +5,11 @@ set -euxo pipefail
 pytest -q -ra --disable-warnings --pyargs numba_dppy -vv
 
 if [[ -v ONEAPI_ROOT ]]; then
+    set +u
     # shellcheck disable=SC1091
     source "${ONEAPI_ROOT}/compiler/latest/env/vars.sh"
+    set -u
+
     export NUMBA_DPPY_LLVM_SPIRV_ROOT="${ONEAPI_ROOT}/compiler/latest/linux/bin"
     echo "Using llvm-spirv from oneAPI"
 else
