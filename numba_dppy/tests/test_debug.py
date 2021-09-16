00749e937a4aab5771900993f77c9fd2efe4982c
elena.totmenina <elena.totmenina@intel.com>
2021-09-16 15:47:01 +0300

    WIP tests debug

---
+++ numba_dppy/tests/test_debug.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+# Copyright 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+import subprocess
+from subprocess import Popen, PIPE
+import os
+import pytest
+
+
+def make_check(text, val_to_search):
+    m = re.search(val_to_search, text, re.I)
+    got = m is not None
+    return got
+
+
+def utils():
+    wd = os.getcwd() + "/numba_dppy/examples/debug"
+    os.chdir(wd)
+    os.environ["NUMBA_OPT"] = "0"
+
+
+# out = Popen(["ls", "-la", "."], stdout=PIPE)
+def test_backtrace():
+    # utils()
+    previous_dir = os.getcwd()
+    previous_numba_opt = os.environ.get("NUMBA_OPT")
+    os.chdir(previous_dir + "/numba_dppy/examples/debug")
+    os.environ["NUMBA_OPT"] = "0"
+
+    expected_commands = [
+        r"Thread .*",
+        r"Thread .\.. hit Breakpoint ., with SIMD lanes \[.\-.\], __main__::func_sum \(\) at simple_dppy_func.py:..",
+        r".*result = a_in_func \+ b_in_func",
+        r"\#0  __main__::func_sum \(\) at simple_dppy_func.py:..",
+        r"\#1  __main__::kernel_sum \(\) at simple_dppy_func.py:..",
+        r"\[Switching to Thread .* lane .\]",
+        r"Thread .\.. hit Breakpoint 1, with SIMD lanes \[.\-.\], __main__::func_sum \(\) at simple_dppy_func.py:..",
+        r".*result = a_in_func \+ b_in_func",
+        r"Done\.\.\.",
+    ]
+    process = Popen(
+        [
+            "gdb-oneapi",
+            "-q",
+            "-command",
+            "commands/backtrace",
+            "python",
+        ],
+        # cwd=wd,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+
+    output_str = str(output)
+    # print("!!!!!!!!!!!!!", output_str, "!!!!!!!!!!!!!")
+    for command in expected_commands:
+        # print(command)
+        # assert command in output
+        assert make_check(output_str, command)
+
+    os.chdir(previous_dir)
+    if previous_numba_opt:
+        os.environ["NUMBA_OPT"] = previous_numba_opt
+    else:
+        os.environ.pop("NUMBA_OPT")
