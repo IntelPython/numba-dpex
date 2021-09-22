9878ccffbd2659160ec8d89124c66c1625837f70
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-09-22 13:59:26 +0000

    Fix passing strides array to DPNP dot and matmul (#565) 16d73cafa28b3091a38104d50116a722a45f739b

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.0rc1+4.ga460848',
+    VERSION: '0.16.0rc1+5.g16d73ca',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
