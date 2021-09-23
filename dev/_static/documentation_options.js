5591fedf19ef83cc96546acfa47531d3eea7bec3
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-09-23 10:59:22 +0000

    Test numba and numba-dppy API with GDB (#566)
    * Dppy vs numba debug test
    * Example test for GDB using pexpect
    * Skip of pexpect not installed
    * Fix using pathlib
    * Add API selection
    
    Co-authored-by: akharche <angelina.kharchevnikova@intel.com> 3b3e3b4f3434d7061323764f5eeaabbd4f50ebc5

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.0rc1+5.g16d73ca',
+    VERSION: '0.16.0rc1+6.g3b3e3b4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
