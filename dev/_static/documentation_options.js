09742fa6a94ad71249e043c3082f5250d9856739
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-09-27 09:23:31 +0000

    Update CHANGELOG for 0.16.0 (#559) 3744160d0f2e1ab481effe8d783f609722872cc8

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.0rc2+1.gcd0013c',
+    VERSION: '0.16.0rc2+2.g3744160',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
