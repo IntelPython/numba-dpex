a2d05dea635b5063641669d729021c60d5b90761
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-10-14 14:23:26 +0000

    Support Numba 0.55 (master branch) (#583)
    * Allow use numba 0.55 in setup.py
    * Add numba 0.55 in test matrix
    * Update conda recipe
    * Update README and docs
    * Use numba 0.54 or 0.55 in docs c5ef7866a214cbf23bfa2b13e8faab6b2862b5bf

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.0+3.gd6af202',
+    VERSION: '0.16.0+4.gc5ef786',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
