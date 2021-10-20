78fcabc6a2aad06eafcd6cfcbfdd47c016d4a8d0
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-10-20 13:13:00 +0000

    Fix dpctl to 0.10.0 (#590) 29d5bf21e581eaeda3611b459c61ff66c9eb5d34

--- next_release/_static/documentation_options.js
+++ next_release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.0+1.gbfd1e7d',
+    VERSION: '0.16.0+2.g29d5bf2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
