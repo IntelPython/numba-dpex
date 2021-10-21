664be6e79426fe578a0411ff8e94364575a48286
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-10-21 11:09:30 +0000

    Upload packages for release* branches (#593) 0b212b4535c9388e24eeb9bbd6f7d14bc6671d6e

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.1+6.g4752ee6',
+    VERSION: '0.16.1+7.g0b212b4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
