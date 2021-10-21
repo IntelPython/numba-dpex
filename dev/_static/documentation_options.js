2d40b10486a56b0836cf0dfac8eea3c2223ba97f
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-10-21 10:00:52 +0000

    Merge branch 'release0.16' into main 4752ee67351f1db5fa06eadfbcc4cd69c8b0e9cb

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.0+5.ge16aedd',
+    VERSION: '0.16.1+6.g4752ee6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
