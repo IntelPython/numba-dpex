2081bdb428c41b75259cccb1fc9f7540447d742d
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-10-23 21:30:19 +0000

    Update to dpctl 0.11 (#595)
    * Update recipe to dpctl 0.11*
    * Update README
    * Update environment.yml f8b3cc81b65ba75d126c8b3cb603d752eb681c7e

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.0dev0+1.gb15a202',
+    VERSION: '0.17.0dev0+2.gf8b3cc8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
