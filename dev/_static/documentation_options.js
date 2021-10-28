a994f9dec3f5875cea64ca4d95209563551797c6
diptorupd <diptorupd@users.noreply.github.com>
2021-10-28 17:05:41 +0000

    Clean up README. (#604) 8ee42e8b8f78a16fe5ccee65838e78ad1d08dac9

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.0rc1+1.g69cf67a',
+    VERSION: '0.17.0rc1+2.g8ee42e8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
