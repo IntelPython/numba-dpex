33587f9e1527afa539667ff012859b0e5ac01e61
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-11-04 17:26:27 +0000

    Merge branch 'release0.17' into main 06756c70d9c6951811111731648e8814d7acccbc

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.0rc2+1.g90a9a68',
+    VERSION: '0.17.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
