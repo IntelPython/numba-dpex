c2d7ba11a36f7cc47e89a7d1a4fb7e43ef5333c6
reazulhoque <reazulhoque@users.noreply.github.com>
2021-11-04 15:32:10 +0000

    Update CHANGLEOG and Readme.md (#611)
    * Update Known Issue in README
    
    * Update Changelog 90a9a682879cfdaa05e8ad543cfa6c76304045ab

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.0rc1+4.g6b979c1',
+    VERSION: '0.17.0rc2+1.g90a9a68',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
