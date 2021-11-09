11f075c1fe15a3bf28a7a5331913d9b0bdfbec72
reazulhoque <reazulhoque@users.noreply.github.com>
2021-11-09 20:37:50 +0000

    Update clang to icx (#622) 90c411e79b8cd2a11dbe5ae4f5cb31338fc3e4c5

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.0+2.g1f706b5',
+    VERSION: '0.17.0+3.g90c411e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
