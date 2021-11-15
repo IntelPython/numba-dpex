782d07fc1756b0b1f4ac448844c0a1c0da0780bf
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-11-15 09:44:15 -0600

    Use llvm-spirv from bin-llvm during build (#626)

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+# new llvm-spirv location
+# starting from dpcpp_impl_linux-64=2022.0.0=intel_3610
+export PATH=$CONDA_PREFIX/bin-llvm:$PATH
+
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
 
 # Build wheel package
