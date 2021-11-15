5f2eb3e24d2493658a4eb72eb1c5c9fb9c56a9ca
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-11-15 20:36:50 +0000

    Use llvm-spirv from bin-llvm during build on Windows (#627)
    * Use llvm-spirv from bin-llvm during build on Windows
    * Use bin-llvm\clang.exe for compiling spirv
    * Use BUILD_PREFIX\Library\bin-llvm 922786274fc9c867e4b793eaddb201256feece96

--- next_release/_static/documentation_options.js
+++ next_release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.1+1.g782d07f',
+    VERSION: '0.17.1+2.g9227862',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
