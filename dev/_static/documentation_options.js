ff086bb173f3ea10a510f97eead950c2ce77d71e
reazul.hoque <reazul.hoque@intel.com>
2021-11-18 12:51:14 -0600

    update doc

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.652.gc23150e.dirty',
+    VERSION: '0+untagged.653.g5ea019b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
