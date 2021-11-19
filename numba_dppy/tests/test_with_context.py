57a25edc601d83be12af6a65a18234f009e3bbf9
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-11-19 18:42:47 +0300

    Add tests

--- numba_dppy/tests/test_with_context.py
+++ numba_dppy/tests/test_with_context.py
@@ -16,14 +16,52 @@ import unittest
 
 import dpctl
 import numpy as np
+import pytest
 from numba import njit
 from numba.tests.support import captured_stdout
 
-import numba_dppy
 import numba_dppy as dppy
 from numba_dppy import config
 
 from . import _helper
+from ._helper import assert_auto_offloading
+
+skip_no_gpu = pytest.mark.skipif(
+    not _helper.has_gpu_queues(), reason="No GPU platforms available"
+)
+skip_no_cpu = pytest.mark.skipif(
+    not _helper.has_cpu_queues(), reason="No CPU platforms available"
+)
+
+
+filter_strings = [
+    pytest.param("level_zero:gpu:0", marks=skip_no_gpu),
+    pytest.param("opencl:gpu:0", marks=skip_no_gpu),
+    pytest.param("opencl:cpu:0", marks=skip_no_cpu),
+]
+
+
+def scenario(filter_str, context):
+    @njit
+    def func(a, b):
+        return a + b
+
+    a = np.ones((64), dtype=np.int32)
+    b = np.ones((64), dtype=np.int32)
+
+    device = dpctl.SyclDevice(filter_str)
+    with context(device):
+        func(a, b)
+
+
+@pytest.mark.parametrize("filter_str", filter_strings)
+@pytest.mark.parametrize("context", [
+    dppy.offload_to_sycl_device,
+    dpctl.device_context,
+])
+def test_dpctl_device_context_affects_numba_pipeline(filter_str, context):
+    with assert_auto_offloading():
+        scenario(filter_str, context)
 
 
 class TestWithDPPYContext(unittest.TestCase):
