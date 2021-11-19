71214cb04a1c3b0f34ec4f1135871dee9850b61f
diptorupd <diptorupd@users.noreply.github.com>
2021-11-19 23:16:14 +0000

    Change black to only allow 80 char lines. Reformat sources. (#631)
    Changes the black configuration to use 80 char wide lines to be consistent with dpctl and PEP-8. All sources are reformatted. I will open a different PR to ignore this commit from git-blame. 2b9c67489cc8a5c6f13b28ec752b29a20c8c9a5f

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.2+5.g7c89846',
+    VERSION: '0.17.2+6.g2b9c674',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
