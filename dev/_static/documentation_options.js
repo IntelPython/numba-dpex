92b121fbd1309e81401d1994e4bc2d23e9320334
diptorupd <diptorupd@users.noreply.github.com>
2021-11-22 18:17:04 +0000

    Rename the driver module to dpctl_iface. (#633)
    - The name driver does not convey much, let us be explicit
        that the module is the interface to dpctl.
    - Move dpctl_functions.py into dpctl_iface. 0b39be7af6d188a31578b87ce8ca94fc76ca3fa1

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.2+7.geef9e5f',
+    VERSION: '0.17.2+8.g0b39be7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
