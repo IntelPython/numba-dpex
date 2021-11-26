509fa5e70211507e3b19ed103f6e4a1415ddfa79
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-11-26 18:44:10 +0300

    pre-commit

--- numba_dppy/dppy_debuginfo.py
+++ numba_dppy/dppy_debuginfo.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numba as nb
 from llvmlite import ir
 from numba.core.debuginfo import DIBuilder
-import numba as nb
 
 numba_version = tuple(map(int, nb.__version__.split(".")[:2]))
 
