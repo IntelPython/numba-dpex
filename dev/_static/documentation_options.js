3ebf1dad7e32896413964efc539b663d35d3c3a2
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-01 08:53:14 +0000

    Fix skipping for Level Zero device for "with context" test (#646) d248279ad12470decffa3692679a75dbc3bf63dc

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+14.gf67318c',
+    VERSION: '0.18.0dev0+15.gd248279',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
