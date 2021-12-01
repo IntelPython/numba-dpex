54d2edd6d86a11869214b0c8c0d38378bf797ecb
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-01 12:02:29 +0000

    Redesign helper functions (#652) 5b012140aa8b4573e96bd960b98dab773f252979

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+15.gd248279',
+    VERSION: '0.18.0dev0+16.g5b01214',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
