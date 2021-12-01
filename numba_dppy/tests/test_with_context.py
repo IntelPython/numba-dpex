b2fc7a4bd076e6ed1b4dc0787f6f2c668484fa34
elena.totmenina <elena.totmenina@intel.com>
2021-12-01 12:59:21 +0300

    Fix

--- numba_dppy/tests/test_with_context.py
+++ numba_dppy/tests/test_with_context.py
@@ -25,15 +25,15 @@ from . import _helper
 from ._helper import assert_auto_offloading
 
 skip_no_gpu = pytest.mark.skipif(
-    not _helper.has_gpu_queues("opencl"),
+    not _helper.has_opencl_gpu(),
     reason="No opencl GPU platforms available",
 )
 skip_no_cpu = pytest.mark.skipif(
-    not _helper.has_cpu_queues("opencl"),
+    not _helper.has_opencl_cpu(),
     reason="No opencl CPU platforms available",
 )
 skip_no_level_zero = pytest.mark.skipif(
-    not _helper.has_gpu_queues("level_zero"),
+    not _helper.has_level_zero(),
     reason="No level-zero GPU platforms available",
 )
 
