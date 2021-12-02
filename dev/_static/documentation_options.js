b135ab00e503854eb15a51a49dbd626658996194
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-02 09:16:46 +0000

    Add numba_support.py with numba_version (#656) 343901a5cf04eaebdf00bd371f655fddcdad1f24

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+17.g040747f',
+    VERSION: '0.18.0dev0+18.g343901a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
