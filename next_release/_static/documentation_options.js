ac3737ea69ba75263bc1f83813fc29bb78ffdb48
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-02 20:11:23 +0000

    Update CHANGELOG.md d55b2dc44b9532d79c7af21d4ba56c67e90b4651

--- next_release/_static/documentation_options.js
+++ next_release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.17.4rc1+1.gf96ecad',
+    VERSION: '0.17.3+2.gd55b2dc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
