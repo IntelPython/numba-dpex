eb64887bf34d511b3d78f007406460ffa714b494
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-02 07:17:09 +0000

    Support Numba 0.55 (including #7177 for GDB) (#585)
    * Support mark_subprogram() changes and DIBuilder.__init__() changes
    * Support abi-tags in mangler
    * Support mark_subprogram changes in numba(master)
    * Remove sending abi-tags for debuginfo
    * Fix import TargetConfigurationStack
    * Make abi_tags support optional
    * Keep compatibility w/ numba 0.54
    * Make DPPYDIBuilder compatible w/ numba 0.54 and 0.55
    * Test w/ numba 0.55.0dev0=*_782
    * Use different numba packages for py38 and py39
    
    Co-authored-by: Vladislav Perevezentsev <vladislav.perevezentsev@intel.com> 040747f024563f043ab10c2a259cde107fbfc426

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+16.g5b01214',
+    VERSION: '0.18.0dev0+17.g040747f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
