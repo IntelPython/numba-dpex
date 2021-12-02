c1c9225b9436b3666676feb9bb452d8aabcb9e52
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-02 13:07:15 +0000

    Refactor side-by-side debugging scenario (#658) 1ec2809a5cada72bdf136da4d72763375a983929

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+19.gf93ca24',
+    VERSION: '0.18.0dev0+20.g1ec2809',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
