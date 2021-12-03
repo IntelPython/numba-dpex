02f77902cf25b4fc941420d16abc0473d1dd6744
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-03 20:46:19 +0000

    Add skip_no_numba055 decorator (#662) dfa316e093299eb0ef600a51c6190212ab8f2051

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+24.g09f52eb',
+    VERSION: '0.18.0dev0+25.gdfa316e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
