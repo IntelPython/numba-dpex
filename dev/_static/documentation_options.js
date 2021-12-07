e57cb0ae23bf6a0cf4624439d6a5d50e29a42ae4
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-07 07:31:53 +0000

    Add decorator to skip unsupported atomics (#664) 19eb6fe1f402a0de85f7e26986a71c313a1a67cd

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+27.g27f39e2',
+    VERSION: '0.18.0dev0+28.g19eb6fe',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
