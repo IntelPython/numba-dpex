b959f8b6d1d27a1dadce376fd1e40a0124d41620
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-08 09:36:18 +0000

    Support arrays with `__sycl_usm_array_interface__` attribute (#629) d72878aae8373ae04d4f2ef861c9ef4d787bd81a

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+28.g19eb6fe',
+    VERSION: '0.18.0dev0+29.gd72878a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
