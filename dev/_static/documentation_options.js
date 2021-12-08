e7331387b23f32ac058535f3d1ee4f866688666b
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-08 16:06:40 +0000

    Move skips for OpenCL devices to helper (#665)
    * Remove skips for opencl to helper
    * Change all filter_strings_with_skips_for_opencl 5592654b3f7e4a735c070a05b9157482e1a812f5

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+30.g4316efb',
+    VERSION: '0.18.0dev0+31.g5592654',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
