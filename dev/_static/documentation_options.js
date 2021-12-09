0181e26b2b95b42633ff35dec37b5ffb59f28c0f
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-09 19:52:16 +0000

    Support dpctl 0.12 (#669)
    * Update dpctl 0.12
    * Fix includes for dpctl 0.12
    * dpctl 0.12 in environment.yml
    * Keep support for dpctl 0.11
    * Use __has_include
    * Use 0.11*|0.12* in conda recipe
    * Build with dpctl 0.11.0
    * Test with different dpctl in public CI
    * Add dpctl to conda_build_config.yml ad95dd58152c65f03e17d6792993cf9499bffeb3

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+31.g5592654',
+    VERSION: '0.18.0dev0+32.gad95dd5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
