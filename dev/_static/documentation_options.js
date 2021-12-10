58dd5c7ad79cfc7dec4e36a4b56fc916d5f08261
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-10 17:15:02 +0000

    Add check for DPNP and pin MKL version (#648)
    * Add test for import DPNP in separate step
    * Pin mkl version for linux testing
    * Pin mkl version in dev env
    * Add CPU device for DPNP or it crashes f24e53396061a7900e3ec9cd1420d2381c718a5b

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+34.g70a611e',
+    VERSION: '0.18.0dev0+35.gf24e533',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
