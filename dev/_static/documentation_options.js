88cc8a6d6295e94ec040821566e8ce8edda4d075
PokhodenkoSA <PokhodenkoSA@users.noreply.github.com>
2021-12-11 22:53:27 +0000

    Add CODEOWNERS for distributing review process (#670)
    * Init CODEOWNERS
    * Add global owner
    * Add code owners for conda-recipe 55205592a80b1ca42dd0450f55881dfd01788652

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0dev0+35.gf24e533',
+    VERSION: '0.18.0dev0+36.g5520559',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
