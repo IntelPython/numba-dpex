69603bc1e9c3ca5f0d411ab6d936148446d2061f
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2021-12-17 10:42:09 +0300

    Add docstring for test_breakpoints.py module

--- numba_dppy/tests/debugging/test_breakpoints.py
+++ numba_dppy/tests/debugging/test_breakpoints.py
@@ -12,6 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests for Setting Breakpoints
+
+https://www.sourceware.org/gdb/onlinedocs/gdb/Set-Breaks.html
+"""
+
 
 import pytest
 
