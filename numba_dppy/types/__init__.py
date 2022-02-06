5a7458b804edf49081ea39f80644286b40f88ff8
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2022-02-06 23:00:30 +0300

    Update types/__init__.py

--- numba_dppy/types/__init__.py
+++ numba_dppy/types/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import dpnp_models, dpnp_typeof
+from . import dpnp_boxing, dpnp_llvm_registration, dpnp_models, dpnp_typeof
 from .dpnp_types import *
