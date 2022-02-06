5cf05cebb0ef6facd19618b7f5b420e8ea50d869
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2022-02-06 22:28:45 +0300

    Add dpnp_ndarray_Model and tests for it

--- numba_dppy/types/dpnp_models.py
+++ numba_dppy/types/dpnp_models.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from numba import types
+from numba.core.datamodel import StructModel
 from numba.core.datamodel.models import ArrayModel
 from numba.extending import register_model
 
@@ -19,7 +21,40 @@ from numba_dppy.target import spirv_data_model_manager
 
 from .dpnp_types import dpnp_ndarray_Type
 
+# we reuse models.ArrayModel
+# it should contain all properties from __sycl_usm_array_interface__
+# __sycl_usm_array_interface__ and __array_interface__ are different
+# _Memory.__sycl_usm_array_interface__ has data, shape, strides, typestr, version, syclobj
+
+
+class dpnp_ndarray_Model(StructModel):
+    """Model for DPNP array.
+
+    It contains all items from models.ArrayModel and adding properties from
+    __sycl_usm_array_interface__: syclobj
+    """
+
+    def __init__(self, dmm, fe_type):
+        ndim = fe_type.ndim
+        members = [
+            # from models.ArrayModel
+            ("meminfo", types.MemInfoPointer(fe_type.dtype)),
+            ("parent", types.pyobject),
+            ("nitems", types.intp),
+            ("itemsize", types.intp),
+            ("data", types.CPointer(fe_type.dtype)),
+            # from __sycl_usm_array_interface__
+            ("syclobj", types.pyobject),
+            # from models.ArrayModel
+            ("shape", types.UniTuple(types.intp, ndim)),
+            ("strides", types.UniTuple(types.intp, ndim)),
+        ]
+        super().__init__(dmm, fe_type, members)
+
+
 # This tells Numba to use the default Numpy ndarray data layout for
 # object of type dpnp.ndarray.
-register_model(dpnp_ndarray_Type)(ArrayModel)
+# register_model(dpnp_ndarray_Type)(ArrayModel)
+
+register_model(dpnp_ndarray_Type)(dpnp_ndarray_Model)
 spirv_data_model_manager.register(dpnp_ndarray_Type, ArrayModel)
