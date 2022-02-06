540fd9294bbbe186cd5c46c7d4c388a0a3e7c895
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2022-02-06 21:13:56 +0300

    Create type and model for dpnp.ndarray
    * Add types/__init__.py for dpnp
    * Add tests for dpnp
    * xfail because no model yet
    * Add dpnp.ndarray model
    * Update types/__init__.py
    * Add model for dpnp.ndarray and extend numba type
    * Add as_array and box_type to dpnp_ndarray_Type
    * Add dpnp_ndarray_Model and tests for it
    * Rename dpnp_ndarray/test_dpnp_ndarray_type.py

---
+++ numba_dppy/types/dpnp_models.py
@@ -0,0 +1,60 @@
+# Copyright 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from numba import types
+from numba.core.datamodel import StructModel
+from numba.core.datamodel.models import ArrayModel
+from numba.extending import register_model
+
+from numba_dppy.target import spirv_data_model_manager
+
+from .dpnp_types import dpnp_ndarray_Type
+
+# we reuse models.ArrayModel
+# it should contain all properties from __sycl_usm_array_interface__
+# __sycl_usm_array_interface__ and __array_interface__ are different
+# _Memory.__sycl_usm_array_interface__ has data, shape, strides, typestr, version, syclobj
+
+
+class dpnp_ndarray_Model(StructModel):
+    """Model for DPNP array.
+
+    It contains all items from models.ArrayModel and adding properties from
+    __sycl_usm_array_interface__: syclobj
+    """
+
+    def __init__(self, dmm, fe_type):
+        ndim = fe_type.ndim
+        members = [
+            # from models.ArrayModel
+            ("meminfo", types.MemInfoPointer(fe_type.dtype)),
+            ("parent", types.pyobject),
+            ("nitems", types.intp),
+            ("itemsize", types.intp),
+            ("data", types.CPointer(fe_type.dtype)),
+            # from __sycl_usm_array_interface__
+            ("syclobj", types.pyobject),
+            # from models.ArrayModel
+            ("shape", types.UniTuple(types.intp, ndim)),
+            ("strides", types.UniTuple(types.intp, ndim)),
+        ]
+        super().__init__(dmm, fe_type, members)
+
+
+# This tells Numba to use the default Numpy ndarray data layout for
+# object of type dpnp.ndarray.
+# register_model(dpnp_ndarray_Type)(ArrayModel)
+
+register_model(dpnp_ndarray_Type)(dpnp_ndarray_Model)
+spirv_data_model_manager.register(dpnp_ndarray_Type, ArrayModel)
---
+++ numba_dppy/types/dpnp_typeof.py
@@ -0,0 +1,45 @@
+# Copyright 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from numba.extending import typeof_impl
+from numba.np import numpy_support
+
+from .dpnp_types import dpnp_ndarray_Type, ndarray
+
+
+# This tells Numba how to create a UsmSharedArrayType when a usmarray is passed
+# into a njit function.
+@typeof_impl.register(ndarray)
+def typeof_dpnp_ndarray(val, c):
+    try:
+        dtype = numpy_support.from_dtype(val.dtype)
+    except NotImplementedError:
+        raise ValueError("Unsupported array dtype: %s" % (val.dtype,))
+
+    try:
+        layout = numpy_support.map_layout(val)
+    except AttributeError:
+        try:
+            # passing nested object as dpnp.ndarray does not support flags yet
+            layout = numpy_support.map_layout(val._array_obj)
+        except TypeError:
+            layout = "C"
+
+    try:
+        readonly = not val.flags.writeable
+    except AttributeError:
+        # dpnp.ndarray does not support flags
+        readonly = False
+
+    return dpnp_ndarray_Type(dtype, val.ndim, layout, readonly=readonly)
