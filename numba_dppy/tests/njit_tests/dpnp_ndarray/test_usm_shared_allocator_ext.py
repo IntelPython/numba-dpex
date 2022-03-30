df75dc72b5ffe414249e5b29c0ad353e37019f1a
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2022-03-30 17:20:31 +0300

    Move test_usm_shared_allocator_ext.py to dpnp_ndarray

--- numba_dppy/tests/njit_tests/dpnp_ndarray/test_usm_shared_allocator_ext.py
+++ numba_dppy/tests/njit_tests/dpnp_ndarray/test_usm_shared_allocator_ext.py
@@ -1,10 +1,10 @@
 """Tests for numba_dppy._usm_shared_allocator_ext
 """
 
-from numba_dppy import _usm_shared_allocator_ext
-
 
 def test_members():
+    from numba_dppy import _usm_shared_allocator_ext
+
     members = ["c_helpers", "get_external_allocator"]
 
     for member in members:
@@ -12,6 +12,8 @@ def test_members():
 
 
 def test_c_helpers():
+    from numba_dppy._usm_shared_allocator_ext import c_helpers
+
     functions = [
         "usmarray_get_ext_allocator",
         "create_allocator",
@@ -20,11 +22,11 @@ def test_c_helpers():
         "create_queue",
     ]
 
-    assert len(functions) == len(_usm_shared_allocator_ext.c_helpers)
+    assert len(functions) == len(c_helpers)
 
     for fn_name in functions:
-        assert fn_name in _usm_shared_allocator_ext.c_helpers
-        assert isinstance(_usm_shared_allocator_ext.c_helpers[fn_name], int)
+        assert fn_name in c_helpers
+        assert isinstance(c_helpers[fn_name], int)
 
 
 def test_allocator():
@@ -40,8 +42,11 @@ def test_allocator():
             ("opaque_data", c_void_p),
         ]
 
-    create_allocator = PYFUNCTYPE(POINTER(NRT_ExternalAllocator), c_int)(c_helpers["create_allocator"])
-    release_allocator = PYFUNCTYPE(None, c_void_p)(c_helpers["release_allocator"])
+    fnty = PYFUNCTYPE(POINTER(NRT_ExternalAllocator), c_int)
+    create_allocator = fnty(c_helpers["create_allocator"])
+
+    fnty = PYFUNCTYPE(None, c_void_p)
+    release_allocator = fnty(c_helpers["release_allocator"])
 
     for usm_type in (0, 1, 2):
         allocator = create_allocator(usm_type)
@@ -75,9 +80,13 @@ def test_meminfo():
             ("external_allocator", c_void_p),
         ]
 
-    DPRT_MemInfo_new = PYFUNCTYPE(POINTER(MemInfo), c_size_t, c_int, c_void_p)(c_helpers["DPRT_MemInfo_new"])
+    fnty = PYFUNCTYPE(POINTER(MemInfo), c_size_t, c_int, c_void_p)
+    DPRT_MemInfo_new = fnty(c_helpers["DPRT_MemInfo_new"])
+
     create_queue = PYFUNCTYPE(c_void_p)(c_helpers["create_queue"])
-    MemInfo_release = PYFUNCTYPE(None, POINTER(MemInfo))(_nrt_python.c_helpers["MemInfo_release"])
+
+    fnty = PYFUNCTYPE(None, POINTER(MemInfo))
+    MemInfo_release = fnty(_nrt_python.c_helpers["MemInfo_release"])
 
     for usm_type in (0, 1, 2):
         queue = create_queue()
