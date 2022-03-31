cea78b461691e42ba7b8c4f77b90746dbdfc45f2
Sergey Pokhodenko <sergey.pokhodenko@intel.com>
2022-03-31 15:52:00 +0300

    Clean imports

--- numba_dppy/dpnp_ndarray/dpnp_empty.py
+++ numba_dppy/dpnp_ndarray/dpnp_empty.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import builtins
-
 import dpnp
 from llvmlite import ir
 from numba import types
