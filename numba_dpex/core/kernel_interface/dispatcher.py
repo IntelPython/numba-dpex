f91e278eb79a673cc12d03f92fec2ef54a5412ba
Diptorup Deb <diptorup.deb@intel.com>
2022-10-17 22:47:17 -0500

    Refactored kernel dispatch API.
      - The concept of a kernel was decoupled from the notion of
        dispatching of a kernel. The present implementation in
        compiler.py intermixes both things, making hard the
        separation of compute-follows-data based kernel launch
        and legacy `dpctl.device_context` based behavior.
      - Deprecates support for numpy arrays as kernel args.
      - Deprecates support for the square bracket notation using
        `__getitem__` to provide global and local ranges for a
        kernel launch.
      - Changes the behavior of specializing a kernel using only
        a signature. The new way to specialize will require a
        device type and a backend.
      - Improvements to exception messages using custom exceptions.
      - The new API is now inside `numba_dpex.core.kernel_interface`.

---
+++ numba_dpex/core/kernel_interface/dispatcher.py
@@ -0,0 +1,404 @@
+# SPDX-FileCopyrightText: 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import copy
+from warnings import warn
+
+import dpctl
+import dpctl.program as dpctl_prog
+from numba.core.types import Array as ArrayType
+
+from numba_dpex import config
+from numba_dpex.core.descriptor import dpex_target
+from numba_dpex.core.exceptions import (
+    ComputeFollowsDataInferenceError,
+    ExecutionQueueInferenceError,
+    InvalidKernelLaunchArgsError,
+    UnknownGlobalRangeError,
+    UnsupportedBackendError,
+)
+from numba_dpex.core.kernel_interface.arg_pack_unpacker import Packer
+from numba_dpex.core.kernel_interface.spirv_kernel import SpirvKernel
+from numba_dpex.dpctl_iface import USMNdArrayType
+
+
+class Dispatcher(object):
+    """Creates a Kernel object from a @kernel decorated function and enqueues
+    the Kernel object on a specified device.
+    """
+
+    # The list of SYCL backends supported by the Dispatcher
+    _supported_backends = ["opencl", "level_zero"]
+
+    def __init__(
+        self,
+        pyfunc,
+        debug_flags=None,
+        compile_flags=None,
+        array_access_specifiers=None,
+    ):
+        self.typingctx = dpex_target.typing_context
+        self.pyfunc = pyfunc
+        self.debug_flags = debug_flags
+        self.compile_flags = compile_flags
+        self.kernel_name = pyfunc.__name__
+        self._global_range = None
+        self._local_range = None
+
+        if array_access_specifiers:
+            warn(
+                "Access specifiers apply only to NumPy ndarrays. "
+                + "Support for NumPy ndarray objects as kernel arguments "
+                + "and access specifiers flags is deprecated. "
+                + "Use dpctl.tensor.usm_ndarray based arrays instead.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            self.array_access_specifiers = array_access_specifiers
+
+        if debug_flags or config.OPT == 0:
+            # if debug is ON we need to pass additional
+            # flags to igc.
+            self._create_sycl_kernel_bundle_flags = ["-g", "-cl-opt-disable"]
+        else:
+            self._create_sycl_kernel_bundle_flags = []
+
+    # def _ensure_valid_work_item_grid(self, val, sycl_queue):
+
+    #     if not isinstance(val, (tuple, list, int)):
+    #         error_message = (
+    #             "Cannot create work item dimension from provided argument"
+    #         )
+    #         raise ValueError(error_message)
+
+    #     if isinstance(val, int):
+    #         val = [val]
+
+    #     # TODO: we need some way to check the max dimensions
+    #     """
+    #     if len(val) > device_env.get_max_work_item_dims():
+    #         error_message = ("Unsupported number of work item dimensions ")
+    #         raise ValueError(error_message)
+    #     """
+
+    #     return list(
+    #         val[::-1]
+    #     )  # reversing due to sycl and opencl interop kernel range mismatch semantic
+
+    def _determine_compute_follows_data_queue(self, usm_array_list):
+        """Determine the execution queue for the list of usm array args using
+        compute follows data rules.
+
+        Uses ``dpctl.utils.get_execution_queue()`` to check if the list of
+        queues belonging to the usm_ndarrays are equivalent. If the queues are
+        equivalent, then returns the queue. If the queues are not equivalent
+        then returns None.
+
+        Args:
+            usm_array_list : A list of usm_ndarray objects
+
+        Returns:
+            A queue the common queue used to allocate the arrays. If no such
+            queue exists, then returns None.
+        """
+
+        queues = [usm_array.sycl_queue for usm_array in usm_array_list]
+        return dpctl.utils.get_execution_queue(queues)
+
+    def _determine_kernel_launch_queue(self, args, argtypes):
+        """Determines the queue where the kernel is to be launched.
+
+        The execution queue is derived using the following algorithm. In future,
+        support for ``numpy.ndarray`` and ``dpctl.device_context`` is to be
+        removed and queue derivation will follows Python Array API's
+        "compute follows data" logic.
+
+        Check if there are array arguments.
+        True:
+          Check if all array arguments are of type numpy.ndarray
+          (numba.types.Array)
+              True:
+                  Check if the kernel was invoked from within a
+                  dpctl.device_context.
+                  True:
+                      Provide a deprecation warning for device_context use and
+                      point to using dpctl.tensor.usm_ndarray or dpnp.ndarray
+
+                      return dpctl.get_current_queue
+                  False:
+                      Raise ExecutionQueueInferenceError
+              False:
+                  Check if all of the arrays are USMNdarray
+                      True:
+                          Check if execution queue could be inferred using
+                          compute follows data rules
+                          True:
+                              return the compute follows data inferred queue
+                          False:
+                              Raise ComputeFollowsDataInferenceError
+                      False:
+                          Raise ComputeFollowsDataInferenceError
+        False:
+          Check if the kernel was invoked from within a dpctl.device_context.
+            True:
+                Provide a deprecation warning for device_context use and
+                point to using dpctl.tensor.usm_ndarray of dpnp.ndarray
+
+                return dpctl.get_current_queue
+            False:
+                Raise ExecutionQueueInferenceError
+
+        Args:
+            args : A list of arguments passed to the kernel stored in the
+            launcher.
+            argtypes : The Numba inferred type for each argument.
+
+        Returns:
+            A queue the common queue used to allocate the arrays. If no such
+            queue exists, then raises an Exception.
+
+        Raises:
+            ComputeFollowsDataInferenceError: If the queue could not be inferred
+                using compute follows data rules.
+            ExecutionQueueInferenceError: If the queue could not be inferred
+                using the dpctl queue manager.
+        """
+        # Temporary workaround as USMNdArrayType derives from Array
+        array_argnums = [
+            i
+            for i, arg in enumerate(args)
+            if isinstance(argtypes[i], ArrayType)
+            and not isinstance(argtypes[i], USMNdArrayType)
+        ]
+        usmarray_argnums = [
+            i
+            for i, arg in enumerate(args)
+            if isinstance(argtypes[i], USMNdArrayType)
+        ]
+
+        # if usm and non-usm array arguments are getting mixed, then the
+        # execution queue cannot be inferred using compute follows data rules.
+        if array_argnums and usmarray_argnums:
+            raise ComputeFollowsDataInferenceError(
+                array_argnums, usmarray_argnum_list=usmarray_argnums
+            )
+        elif array_argnums and not usmarray_argnums:
+            if dpctl.is_in_device_context():
+                warn(
+                    "Support for dpctl.device_context to specify the "
+                    + "execution queue is deprecated. "
+                    + "Use dpctl.tensor.usm_ndarray based array "
+                    + "containers instead. ",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                warn(
+                    "Support for NumPy ndarray objects as kernel arguments is "
+                    + "deprecated. Use dpctl.tensor.usm_ndarray based array "
+                    + "containers instead. ",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                return dpctl.get_current_queue()
+            else:
+                raise ExecutionQueueInferenceError(self.kernel.name)
+        elif usmarray_argnums and not array_argnums:
+            if dpctl.is_in_device_context():
+                warn(
+                    "dpctl.device_context ignored as the kernel arguments "
+                    + "are dpctl.tensor.usm_ndarray based array containers."
+                )
+            usm_array_args = [
+                arg for i, arg in enumerate(args) if i in usmarray_argnums
+            ]
+            queue = self._determine_compute_follows_data_queue(usm_array_args)
+            if not queue:
+                raise ComputeFollowsDataInferenceError(
+                    self.kernel.name, usmarray_argnums
+                )
+            else:
+                return queue
+        else:
+            if dpctl.is_in_device_context():
+                warn(
+                    "Support for dpctl.device_context to specify the "
+                    + "execution queue is deprecated. "
+                    + "Use dpctl.tensor.usm_ndarray based array "
+                    + "containers instead. ",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                return dpctl.get_current_queue()
+            else:
+                raise ExecutionQueueInferenceError(self.kernel.name)
+
+    def __getitem__(self, args):
+        """Mimic's ``numba.cuda`` square-bracket notation for configuring the
+        global_range and local_range settings when launching a kernel on a
+        SYCL queue.
+
+        When a Python function decorated with the @kernel decorator,
+        is invoked it creates a KernelLauncher object. Calling the
+        KernelLauncher objects ``__getitem__`` function inturn clones the object
+        and sets the ``global_range`` and optionally the ``local_range``
+        attributes with the arguments passed to ``__getitem__``.
+
+        Args:
+            args (tuple): A tuple of tuples that specify the global and
+            optionally the local range for the kernel execution. If the
+            argument is a two-tuple of tuple, then it is assumed that both
+            global and local range options are specified. The first entry is
+            considered to be the global range and the second the local range.
+
+            If only a single tuple value is provided, then the kernel is
+            launched with only a global range and the local range configuration
+            is decided by the SYCL runtime.
+
+        Returns:
+            KernelLauncher: A clone of the KernelLauncher object, but with the
+            global_range and local_range attributes initialized.
+
+        .. deprecated:: 0.19
+            Use :func:`KernelLauncher.execute` instead.
+        """
+
+        warn(
+            "The [] (__getitem__) method to set global and local ranges for "
+            + "launching a kernel is deprecated. "
+            + "Use the execute function instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+
+        nargs = len(args)
+
+        # Check if the kernel launch arguments are sane.
+        if nargs < 1:
+            raise UnknownGlobalRangeError(kernel_name=self.kernel_name)
+        elif nargs > 2:
+            raise InvalidKernelLaunchArgsError(
+                kernel_name=self.kernel_name, args=args
+            )
+
+        self.global_range = args[0]
+        if nargs == 2 and args[1] != []:
+            self.local_range = args[1]
+        else:
+            self.local_range = None
+
+        return copy.copy(self)
+
+    def _get_ranges(self, global_range, local_range):
+        """_summary_
+
+        Args:
+            global_range (_type_): _description_
+            local_range (_type_): _description_
+
+        Raises:
+            UnknownGlobalRangeError: _description_
+        """
+        if global_range:
+            if self._global_range:
+                warn(
+                    "Ignoring the previously set value of global_range and "
+                    + "using the value specified at the kernel call site."
+                )
+        else:
+            if self._global_range:
+                warn(
+                    "Use of __getitem__ to set the global_range attribute is "
+                    + 'deprecated. Use the keyword argument "global_range" of '
+                    + "__call__ method to set the attribute."
+                )
+                global_range = self._global_range
+
+            else:
+                raise UnknownGlobalRangeError(self.kernel_name)
+
+        if local_range:
+            if self._local_range:
+                warn(
+                    "Ignoring the previously set value of local_range and "
+                    + "using the value specified at the kernel call site.."
+                )
+        else:
+            if self._local_range:
+                warn(
+                    "Use of __getitem__ to set the local_range attribute is "
+                    + 'deprecated. Use the keyword argument "local_range" of '
+                    + "__call__ method to set the attribute."
+                )
+                local_range = self._local_range
+            else:
+                local_range = None
+                warn(
+                    "Kernel to be submitted without a local range letting "
+                    + "the SYCL runtime select a local range. The behavior "
+                    + "can lead to suboptimal performance in certain cases. "
+                    + "Consider setting the local range value for the kernel "
+                    + "execution.\n"
+                    + "The local_range keyword may be made a required argument "
+                    + "in the future."
+                )
+
+        # TODO: Test global and local ranges to be valid for the device
+
+        return (global_range, local_range)
+
+    def __call__(self, *args, global_range, local_range=None):
+        """_summary_
+
+        Args:
+            global_range (_type_): _description_
+            local_range (_type_): _description_.
+        """
+        argtypes = [self.typingctx.resolve_argument_type(arg) for arg in args]
+
+        # FIXME: Remove along with __getitem__
+        global_range, local_range = self._get_ranges(global_range, local_range)
+
+        exec_queue = self._determine_kernel_launch_queue(args, argtypes)
+        backend = exec_queue.backend
+
+        if exec_queue.backend not in [
+            dpctl.backend_type.opencl,
+            dpctl.backend_type.level_zero,
+        ]:
+            raise UnsupportedBackendError(
+                self.kernel_name, backend, Dispatcher._supported_backends
+            )
+
+        # TODO: Enable caching of kernels, but do it using Numba's caching
+        # machinery
+
+        kernel = SpirvKernel(self.pyfunc, self.kernel_name)
+        kernel.compile(
+            arg_types=argtypes,
+            debug=self.debug_flags,
+            extra_compile_flags=self.compile_flags,
+        )
+
+        # create a sycl::KernelBundle
+        kernel_bundle = dpctl_prog.create_program_from_spirv(
+            exec_queue,
+            kernel.device_driver_ir_module,
+            " ".join(self._create_sycl_kernel_bundle_flags),
+        )
+        #  get the sycl::kernel
+        kernel = kernel_bundle.get_sycl_kernel(kernel.module_name)
+
+        packer = Packer(arg_list=args, argty_list=argtypes, queue=exec_queue)
+
+        exec_queue.submit(
+            kernel,
+            packer.unpacked_args,
+            global_range,
+            local_range,
+        )
+
+        exec_queue.wait()
+
+        # TODO remove once NumPy support is removed
+        packer.repacked_args
