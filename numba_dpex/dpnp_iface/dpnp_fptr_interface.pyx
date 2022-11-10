c9fd2c93b2b8f57fb743958fc750af1705e2acaa
Diptorup Deb <diptorup.deb@intel.com>
2022-11-10 02:12:24 -0600

    Minor formatting fixes.

--- numba_dpex/dpnp_iface/dpnp_fptr_interface.pyx
+++ numba_dpex/dpnp_iface/dpnp_fptr_interface.pyx
@@ -8,7 +8,8 @@
 import ctypes
 
 
-cdef extern from "dpnp_iface_fptr.hpp" namespace "DPNPFuncName":  # need this namespace for Enum import
+cdef extern from "dpnp_iface_fptr.hpp" namespace "DPNPFuncName":
+    # The namespace is needed to import Enum
     cdef enum DPNPFuncName "DPNPFuncName":
         DPNP_FN_ABSOLUTE
         DPNP_FN_ADD
@@ -121,7 +122,8 @@ cdef extern from "dpnp_iface_fptr.hpp" namespace "DPNPFuncName":  # need this na
         DPNP_FN_VAR
 
 
-cdef extern from "dpnp_iface_fptr.hpp" namespace "DPNPFuncType":  # need this namespace for Enum import
+cdef extern from "dpnp_iface_fptr.hpp" namespace "DPNPFuncType":
+    # The namespace is needed to import Enum
     cdef enum DPNPFuncType "DPNPFuncType":
         DPNP_FT_NONE
         DPNP_FT_INT
@@ -135,7 +137,9 @@ cdef extern from "dpnp_iface_fptr.hpp":
         DPNPFuncType return_type
         void * ptr
 
-    DPNPFuncData get_dpnp_function_ptr(DPNPFuncName name, DPNPFuncType first_type, DPNPFuncType second_type) except +
+    DPNPFuncData get_dpnp_function_ptr(
+        DPNPFuncName name, DPNPFuncType first_type, DPNPFuncType second_type
+    ) except +
 
 
 
@@ -273,7 +277,9 @@ cpdef get_dpnp_fn_ptr(name, types):
     cdef DPNPFuncType first_type = get_DPNPFuncType_from_str(types[0])
     cdef DPNPFuncType second_type = get_DPNPFuncType_from_str(types[1])
 
-    cdef DPNPFuncData kernel_data = get_dpnp_function_ptr(func_name, first_type, second_type)
+    cdef DPNPFuncData kernel_data = get_dpnp_function_ptr(
+        func_name, first_type, second_type
+    )
     cdef uintptr_t fn_ptr = <uintptr_t>kernel_data.ptr
 
     return <object>fn_ptr
