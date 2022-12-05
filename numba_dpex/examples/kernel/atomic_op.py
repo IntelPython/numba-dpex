06bbbf412cddad5c5cb085ce1273d20904f8902f
samaid <sergey.maidanov@intel.com>
2022-12-05 13:10:21 -0600

    Refactored atomic_op.py example

---
+++ numba_dpex/examples/kernel/atomic_op.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2020 - 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import dpnp as np
+
+import numba_dpex as ndpex
+
+N = 10
+
+
+@ndpex.kernel
+def atomic_reduction(a):
+    idx = ndpex.get_global_id(0)
+    ndpex.atomic.add(a, 0, a[idx])
+
+
+def main():
+    try:
+        a = np.arange(N, device="gpu")
+    except:
+        print("No GPU device")
+
+    print("Using device ...")
+    print(a.device)
+    print("a=", a)
+
+    atomic_reduction[N, ndpex.DEFAULT_LOCAL_SIZE](a)
+    print("Reduction sum =", a[0])
+
+    print("Done...")
+
+
+if __name__ == "__main__":
+    main()
