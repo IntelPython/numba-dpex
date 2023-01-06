539adffd75251b52f972c59d53dcb5565122f4b9
Diptorup Deb <diptorup.deb@intel.com>
2023-01-06 12:00:42 -0600

    Mark all vectorize tests as xfail.

--- numba_dpex/tests/test_vectorize.py
+++ numba_dpex/tests/test_vectorize.py
@@ -23,6 +23,7 @@ def shape(request):
     return request.param
 
 
+@pytest.mark.xfail
 @pytest.mark.parametrize("filter_str", filter_strings)
 def test_njit(filter_str):
     @vectorize(nopython=True)
@@ -66,6 +67,7 @@ def input_type(request):
     return request.param
 
 
+@pytest.mark.xfail
 @pytest.mark.parametrize("filter_str", filter_strings)
 def test_vectorize(filter_str, shape, dtypes, input_type):
     def vector_add(a, b):
