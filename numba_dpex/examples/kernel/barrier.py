af78b70a390668c0ce7068f3eb997bb2ab8c9d1e
Sergey Maydanov <sergey.maidanov@intel.com>
2023-01-18 14:06:57 -0600

    Update barrier.py example

--- numba_dpex/examples/kernel/barrier.py
+++ numba_dpex/examples/kernel/barrier.py
@@ -3,77 +3,68 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import dpctl
-import numpy as np
+import dpnp as np
 from numba import float32
 
 import numba_dpex as dpex
 
 
+# This example demonstrates the usage of numba_dpex's ``barrier``
+# intrinsic function. The ``barrier`` function is usable only inside
+# a ``kernel`` and is equivalent to OpenCL's ``barrier`` function.
 def no_arg_barrier_support():
-    """
-    This example demonstrates the usage of numba_dpex's ``barrier``
-    intrinsic function. The ``barrier`` function is usable only inside
-    a ``kernel`` and is equivalent to OpenCL's ``barrier`` function.
-    """
+    N = 10  # 1D array size
 
     @dpex.kernel
-    def twice(A):
+    def kernel_twice(a):
         i = dpex.get_global_id(0)
-        d = A[i]
-        # no argument defaults to global mem fence
-        dpex.barrier()
-        A[i] = d * 2
+        d = a[i]
 
-    N = 10
-    arr = np.arange(N).astype(np.float32)
-    print(arr)
+        dpex.barrier()  # No argument defaults to global mem fence
 
-    # Use the environment variable SYCL_DEVICE_FILTER to change the default device.
-    # See https://github.com/intel/llvm/blob/sycl/sycl/doc/EnvironmentVariables.md#sycl_device_filter.
-    device = dpctl.select_default_device()
+        a[i] = d * 2
+
+    arr = np.arange(N, dtype=np.float32)
+    print("Original array:", arr)
+
     print("Using device ...")
-    device.print_device_info()
+    arr.device.print_device_info()
 
-    with dpctl.device_context(device):
-        twice[N, dpex.DEFAULT_LOCAL_SIZE](arr)
+    kernel_twice[N, dpex.DEFAULT_LOCAL_SIZE](arr)
 
-    # the output should be `arr * 2, i.e. [0, 2, 4, 6, ...]`
+    # the output should be [0, 2, 4, 6, ...]
     print(arr)
 
 
+# This example demonstrates the usage of numba-dpex's ``local.array``
+# intrinsic function. The function is used to create a static array
+# allocated on the devices local address space.
 def local_memory():
-    """
-    This example demonstrates the usage of numba-dpex's `local.array`
-    intrinsic function. The function is used to create a static array
-    allocated on the devices local address space.
-    """
-    blocksize = 10
+    N = 10  # 1D array size
 
     @dpex.kernel
-    def reverse_array(A):
-        lm = dpex.local.array(shape=10, dtype=float32)
+    def reverse_array(a):
+        lm = dpex.local.array(shape=N, dtype=float32)
         i = dpex.get_global_id(0)
 
         # preload
-        lm[i] = A[i]
+        lm[i] = a[i]
+
         # barrier local or global will both work as we only have one work group
         dpex.barrier(dpex.CLK_LOCAL_MEM_FENCE)  # local mem fence
+
         # write
-        A[i] += lm[blocksize - 1 - i]
+        a[i] += lm[N - 1 - i]
 
-    arr = np.arange(blocksize).astype(np.float32)
-    print(arr)
+    arr = np.arange(N, dtype=np.float32)
+    print("Original array:", arr)
 
-    # Use the environment variable SYCL_DEVICE_FILTER to change the default device.
-    # See https://github.com/intel/llvm/blob/sycl/sycl/doc/EnvironmentVariables.md#sycl_device_filter.
-    device = dpctl.select_default_device()
     print("Using device ...")
-    device.print_device_info()
+    arr.device.print_device_info()
 
-    with dpctl.device_context(device):
-        reverse_array[blocksize, dpex.DEFAULT_LOCAL_SIZE](arr)
+    reverse_array[N, dpex.DEFAULT_LOCAL_SIZE](arr)
 
-    # the output should be `orig[::-1] + orig, i.e. [9, 9, 9, ...]``
+    # the output should be [9, 8, 7, ...]
     print(arr)
 
 
