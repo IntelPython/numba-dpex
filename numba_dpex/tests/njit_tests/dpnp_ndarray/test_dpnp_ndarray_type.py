38953f5d46b210e9a9a84dab72dd8a55ff4db608
Diptorup Deb <diptorup.deb@intel.com>
2023-01-19 23:23:06 -0600

    Remove numpy_usm_shared check from test_dpnp_ndarray_type.

--- numba_dpex/tests/njit_tests/dpnp_ndarray/test_dpnp_ndarray_type.py
+++ numba_dpex/tests/njit_tests/dpnp_ndarray/test_dpnp_ndarray_type.py
@@ -7,18 +7,15 @@ Tests for numba_dpex.dpnp_ndarray.typeof
 """
 
 import pytest
-from dpctl.tensor.numpy_usm_shared import ndarray as dpctl_ndarray
 from dpnp import ndarray as dpnp_ndarray
-from numba import njit, typeof, types
+from numba import typeof
 
 from numba_dpex.core.types.dpnp_ndarray_type import DpnpNdarray
-from numba_dpex.numpy_usm_shared import UsmSharedArrayType
 
 
 @pytest.mark.parametrize(
     "array_type, expected_numba_type",
     [
-        (dpctl_ndarray, UsmSharedArrayType),
         (dpnp_ndarray, DpnpNdarray),
     ],
 )
@@ -31,5 +28,4 @@ from numba_dpex.numpy_usm_shared import UsmSharedArrayType
 )
 def test_typeof(array_type, shape, expected_numba_type, expected_ndim):
     array = array_type(shape)
-    expected_type = expected_numba_type(types.float64, expected_ndim, "C")
-    assert typeof(array) == expected_type
+    assert isinstance(typeof(array), expected_numba_type)
