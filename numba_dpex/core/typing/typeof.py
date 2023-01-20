ca789bcc00582659e4394ca71927ae061961bf91
Diptorup Deb <diptorup.deb@intel.com>
2023-01-19 23:18:21 -0600

    Ensure that the typeof for dpnp.ndarray returns the new DpnpNdarray type.

--- numba_dpex/core/typing/typeof.py
+++ numba_dpex/core/typing/typeof.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from dpctl.tensor import usm_ndarray
+from dpnp import ndarray
 from numba.extending import typeof_impl
 from numba.np import numpy_support
 
@@ -11,22 +12,8 @@ from numba_dpex.core.types.usm_ndarray_type import USMNdArray
 from numba_dpex.utils import address_space
 
 
-@typeof_impl.register(usm_ndarray)
-def typeof_usm_ndarray(val, c):
-    """Registers the type inference implementation function for
-    dpctl.tensor.usm_ndarray
-
-    Args:
-        val : A Python object that should be an instance of a
-        dpctl.tensor.usm_ndarray
-        c : Unused argument used to be consistent with Numba API.
-
-    Raises:
-        ValueError: If an unsupported dtype encountered or val has
-        no ``usm_type`` or sycl_device attribute.
-
-    Returns: The Numba type corresponding to dpctl.tensor.usm_ndarray
-    """
+def _typeof_helper(val, array_class_type):
+    """Creates a Numba type of the specified ``array_class_type`` for ``val``."""
     try:
         dtype = numpy_support.from_dtype(val.dtype)
     except NotImplementedError:
@@ -56,7 +43,7 @@ def typeof_usm_ndarray(val, c):
     except AttributeError:
         raise ValueError("The device for the usm_ndarray could not be inferred")
 
-    return USMNdArray(
+    return array_class_type(
         dtype=dtype,
         ndim=val.ndim,
         layout=layout,
@@ -68,7 +55,38 @@ def typeof_usm_ndarray(val, c):
     )
 
 
-@typeof_impl.register(DpnpNdarray)
-def typeof_dpnp_ndarray(val, c):
+@typeof_impl.register(usm_ndarray)
+def typeof_usm_ndarray(val, c):
+    """Registers the type inference implementation function for
+    dpctl.tensor.usm_ndarray
 
-    return typeof_usm_ndarray(val, c)
+    Args:
+        val : A Python object that should be an instance of a
+        dpctl.tensor.usm_ndarray
+        c : Unused argument used to be consistent with Numba API.
+
+    Raises:
+        ValueError: If an unsupported dtype encountered or val has
+        no ``usm_type`` or sycl_device attribute.
+
+    Returns: The Numba type corresponding to dpctl.tensor.usm_ndarray
+    """
+    return _typeof_helper(val, USMNdArray)
+
+
+@typeof_impl.register(ndarray)
+def typeof_dpnp_ndarray(val, c):
+    """Registers the type inference implementation function for dpnp.ndarray.
+
+    Args:
+        val : A Python object that should be an instance of a
+        dpnp.ndarray
+        c : Unused argument used to be consistent with Numba API.
+
+    Raises:
+        ValueError: If an unsupported dtype encountered or val has
+        no ``usm_type`` or sycl_device attribute.
+
+    Returns: The Numba type corresponding to dpnp.ndarray
+    """
+    return _typeof_helper(val, DpnpNdarray)
