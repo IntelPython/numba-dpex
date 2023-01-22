d801fbe8658c4ae760fc3b2eab1ac970ed468cde
Diptorup Deb <diptorup.deb@intel.com>
2023-01-21 23:50:02 -0600

    Add initial stubs for a DpexTarget for a future dpjit decorator

--- numba_dpex/core/descriptor.py
+++ numba_dpex/core/descriptor.py
@@ -2,10 +2,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from numba.core import utils
+from numba.core import typing, utils
 from numba.core.cpu import CPUTargetOptions
 from numba.core.descriptors import TargetDescriptor
 
+from .targets.dpjit_target import DPEX_TARGET_NAME, DpexTargetContext
 from .targets.kernel_target import (
     DPEX_KERNEL_TARGET_NAME,
     DpexKernelTargetContext,
@@ -14,6 +15,10 @@ from .targets.kernel_target import (
 
 
 class DpexKernelTarget(TargetDescriptor):
+    """
+    Implements a target descriptor for numba_dpex.kernel decorated functions.
+    """
+
     options = CPUTargetOptions
 
     @utils.cached_property
@@ -41,5 +46,40 @@ class DpexKernelTarget(TargetDescriptor):
         return self._toplevel_typing_context
 
 
+class DpexTarget(TargetDescriptor):
+    """
+    Implements a target descriptor for numba_dpex.dpjit decorated functions.
+    """
+
+    options = CPUTargetOptions
+
+    @utils.cached_property
+    def _toplevel_target_context(self):
+        # Lazily-initialized top-level target context, for all threads
+        return DpexTargetContext(self.typing_context, self._target_name)
+
+    @utils.cached_property
+    def _toplevel_typing_context(self):
+        # Lazily-initialized top-level typing context, for all threads
+        return typing.Context()
+
+    @property
+    def target_context(self):
+        """
+        The target context for dpex targets.
+        """
+        return self._toplevel_target_context
+
+    @property
+    def typing_context(self):
+        """
+        The typing context for dpex targets.
+        """
+        return self._toplevel_typing_context
+
+
 # A global instance of the DpexKernelTarget
 dpex_kernel_target = DpexKernelTarget(DPEX_KERNEL_TARGET_NAME)
+
+# A global instance of the DpexTarget
+dpex_target = DpexTarget(DPEX_TARGET_NAME)
