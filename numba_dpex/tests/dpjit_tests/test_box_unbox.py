7fe8d9b85edf0ddf587b35e6a0f696801944ef04
Diptorup Deb <diptorup.deb@intel.com>
2023-02-12 20:57:25 -0600

    Add basic support for dpctl.SyclQueue as a Numba type.
       - Adds a Numba type to represent dpctl.SyclQueue and
         infer it as an opaque pointer inside the compiler.

---
+++ numba_dpex/tests/dpjit_tests/test_box_unbox.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2020 - 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Tests for boxing and unboxing of types supported inside dpjit
+"""
+
+import dpctl
+import pytest
+
+from numba_dpex import dpjit
+
+
+@pytest.mark.parametrize(
+    "obj",
+    [
+        pytest.param(dpctl.SyclQueue()),
+    ],
+)
+def test_boxing_unboxing(obj):
+    @dpjit
+    def func(a):
+        return a
+
+    o = func(obj)
+    assert id(o) == id(obj)
