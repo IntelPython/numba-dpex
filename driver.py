eb716689cf81433aa3a21e97986ab7e280ba0d14
Diptorup Deb <diptorup.deb@intel.com>
2023-02-14 20:09:05 -0600

    Driver.py

---
+++ driver.py
@@ -0,0 +1,21 @@
+import dpnp
+import numpy as np
+
+import numba_dpex as dpex
+
+
+@dpex.dpjit
+def vecadd(a, b):
+    return a + b
+
+
+@dpex.dpjit
+def sin(a):
+    return np.sin(a)
+
+
+a = dpnp.ones(10)
+b = dpnp.ones(10)
+# c = vecadd(a, b)
+
+a = sin(a)
