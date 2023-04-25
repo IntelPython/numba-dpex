3abfe121d2f9e91d8940ea0f835fa8225135379b
Diptorup Deb <diptorup.deb@intel.com>
2023-04-24 20:57:04 -0500

    Move version parsing tests into misc.

---
+++ numba_dpex/tests/misc/test_dpctl_version.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2020 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import dpctl
+
+from numba_dpex import dpctl_version
+
+
+def test_dpctl_version():
+    dpctl_v = dpctl.__version__
+    computed_v = ".".join(str(n) for n in dpctl_version)
+    n = len(computed_v)
+    assert n <= len(dpctl_v)
+    assert computed_v == dpctl_v[:n]
