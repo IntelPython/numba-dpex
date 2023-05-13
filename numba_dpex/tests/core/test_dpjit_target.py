2c21830298ede47430bd04c79f94139d7c4299cd
khaled <akm.khaled.talukder@gmail.com>
2023-05-12 20:10:01 -0500

    Adding unit-test for dpjit target caching

--- numba_dpex/tests/core/test_dpjit_target.py
+++ numba_dpex/tests/core/test_dpjit_target.py
@@ -5,10 +5,13 @@
 """Tests for class DpexTargetContext."""
 
 
+import dpctl
+import dpnp
 import pytest
 from numba.core import typing
 from numba.core.codegen import JITCPUCodegen
 
+from numba_dpex import dpjit
 from numba_dpex.core.targets.dpjit_target import DpexTargetContext
 
 ctx = typing.Context()
@@ -30,3 +33,29 @@ def test_dpjit_target_refresh():
         dpexctx.refresh
     except KeyError:
         pytest.fail("Unexpected KeyError in dpjit_target.")
+
+
+def test_dpjit_target_caching():
+    @dpjit(cache=True)
+    def func(a):
+        b = dpnp.ones(10)
+        return a + b
+
+    q1 = dpctl.SyclQueue()
+    q2 = dpctl.SyclQueue()
+
+    b = dpnp.zeros(10, sycl_queue=q1)
+    c = func(b)
+    print(c)
+
+    b = dpnp.ones(10, sycl_queue=q2)
+    c = func(b)
+    print(c)
+
+    b = dpnp.ones(5)
+    c = func(b)
+    print(c)
+
+    b = dpnp.ones(10, sycl_queue=q2, dtype=dpnp.int32)
+    c = func(b)
+    print(c)
