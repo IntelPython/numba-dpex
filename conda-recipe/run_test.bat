ac0e1f344abc6e317010a4fa10814b08d74021e6
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2023-06-15 18:12:17 -0400

    Run tests for every sycl selector

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,4 +1,12 @@
-pytest -q -ra --disable-warnings --pyargs numba_dpex -vv
-IF %ERRORLEVEL% NEQ 0 exit /B 1
+set "ONEAPI_DEVICE_SELECTOR="
+
+for /F "USEBACKQ tokens=* delims=" %%F in (
+`python -c "import dpctl; print(\"\n\".join([dev.backend.name+\":\"+dev.device_type.name for dev in dpctl.get_devices() if dev.device_type.name in [\"cpu\",\"gpu\"]]))"`
+) do (
+    set "ONEAPI_DEVICE_SELECTOR=%%F"
+
+    pytest -q -ra --disable-warnings --pyargs numba_dpex -vv
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
+)
 
 exit /B 0
