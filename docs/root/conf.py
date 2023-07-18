b34f6e1ef69ad25b1a8095ad461fda431ad9949b
khaled <akm.khaled.talukder@intel.com>
2023-07-18 11:20:49 -0500

    Render markdown in release-notes.rst

--- docs/root/conf.py
+++ docs/root/conf.py
@@ -52,6 +52,7 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinxcontrib.programoutput",
     "sphinxcontrib.googleanalytics",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
