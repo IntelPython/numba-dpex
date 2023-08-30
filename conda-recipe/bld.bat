752455575016b2f853f458edb5b4b3feaab5698f
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2023-08-30 19:05:34 +0200

    Use ninja

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -3,11 +3,14 @@
 @REM used BUILD_PREFIX as compiler installed in build section of meta.yml
 set "PATH=%BUILD_PREFIX%\Library\bin-llvm;%PATH%"
 
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
+set "SKBUILD_ARGS=-G Ninja --"
+set "SKBUILD_ARGS=%SKBUILD_ARGS% -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+
+%PYTHON% setup.py install --single-version-externally-managed --record=record.txt %SKBUILD_ARGS%
 
 rem Build wheel package
 if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
-    %PYTHON% setup.py bdist_wheel --build-number %GIT_DESCRIBE_NUMBER%
+    %PYTHON% setup.py bdist_wheel --build-number %GIT_DESCRIBE_NUMBER% %SKBUILD_ARGS%
     if errorlevel 1 exit 1
     copy dist\numba_dpex*.whl %WHEELS_OUTPUT_FOLDER%
     if errorlevel 1 exit 1
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -6,11 +6,13 @@ set -euxo pipefail
 # starting from dpcpp_impl_linux-64=2022.0.0=intel_3610
 export PATH=$CONDA_PREFIX/bin-llvm:$PATH
 
-${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
+SKBUILD_ARGS=(-G Ninja -- -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON)
+
+${PYTHON} setup.py install --single-version-externally-managed --record=record.txt "${SKBUILD_ARGS[@]}"
 
 # Build wheel package
 WHEELS_BUILD_ARGS=(-p manylinux2014_x86_64 --build-number "$GIT_DESCRIBE_NUMBER")
 if [[ -v WHEELS_OUTPUT_FOLDER ]]; then
-    $PYTHON setup.py bdist_wheel "${WHEELS_BUILD_ARGS[@]}"
+    $PYTHON setup.py bdist_wheel "${WHEELS_BUILD_ARGS[@]}" "${SKBUILD_ARGS[@]}"
     cp dist/numba_dpex*.whl "${WHEELS_OUTPUT_FOLDER[@]}"
 fi
