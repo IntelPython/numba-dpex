0b1c7666bf254475f4ffdad4781ce85d54647d7c
Diptorup Deb <diptorup.deb@intel.com>
2023-12-12 19:01:07 -0600

    Enable INLINE_THRESHOLD warning for core kernel compilation.
        - The numba_dpex.core.kernel_interface.disptcher.JitKernel
          is not a true numba Dispatcher class and as such does not
          fully use targetoptions.
    
          As such the commit adds a temporary work around to set the
          code library inline_threshold only using the
          config.INLINE_THRESHOLD environment flag.
    
          Unit tests were updated as well.

--- numba_dpex/tests/misc/test_warnings.py
+++ numba_dpex/tests/misc/test_warnings.py
@@ -1,6 +1,7 @@
-import logging
+import warnings
 
 import dpnp
+import pytest
 
 import numba_dpex as dpex
 import numba_dpex.config as config
@@ -15,28 +16,23 @@ def test_opt_warning(caplog):
     bkp = config.DPEX_OPT
     config.DPEX_OPT = 3
 
-    with caplog.at_level(logging.WARNING):
+    with pytest.warns(UserWarning):
         foo[dpex.Range(10)](dpnp.arange(10))
 
     config.DPEX_OPT = bkp
 
-    assert "NUMBA_DPEX_OPT" in caplog.text
-
 
 def test_inline_warning(caplog):
     bkp = config.INLINE_THRESHOLD
-    config.INLINE_THRESHOLD = 2
+    config.INLINE_THRESHOLD = 3
 
-    with caplog.at_level(logging.WARNING):
+    with pytest.warns(UserWarning):
         foo[dpex.Range(10)](dpnp.arange(10))
 
     config.INLINE_THRESHOLD = bkp
 
-    assert "INLINE_THRESHOLD" in caplog.text
-
 
 def test_no_warning(caplog):
-    with caplog.at_level(logging.WARNING):
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
         foo[dpex.Range(10)](dpnp.arange(10))
-
-    assert caplog.text == ""
