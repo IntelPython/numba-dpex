011675cd8b7b168cd42d20f5508ba81ca1c2f6e0
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-01-05 17:29:22 -0500

    Add dpex target intrinsic test

--- numba_dpex/tests/dpjit_tests/test_dpex_target_overload_isolation.py
+++ numba_dpex/tests/dpjit_tests/test_dpex_target_overload_isolation.py
@@ -8,9 +8,9 @@ available at numba_dpex.dpjit.
 """
 
 import pytest
-from numba import njit
+from numba import njit, types
 from numba.core import errors
-from numba.extending import overload
+from numba.extending import intrinsic, overload
 
 from numba_dpex import dpjit
 from numba_dpex.core.targets.dpjit_target import DPEX_TARGET_NAME
@@ -25,8 +25,36 @@ def ol_foo():
     return lambda: 1
 
 
+@intrinsic(target=DPEX_TARGET_NAME)
+def intrinsic_foo(
+    ty_context,
+):
+    """A numba "intrinsic" function to inject dpctl.SyclEvent constructor code.
+
+    Args:
+        ty_context (numba.core.typing.context.Context): The typing context
+            for the codegen.
+
+    Returns:
+        tuple(numba.core.typing.templates.Signature, function): A tuple of
+            numba function signature type and a function object.
+    """
+
+    sig = types.int32(types.void)
+
+    def codegen(context, builder, sig, args: list):
+        return context.get_constant(types.int32, 1)
+
+    return sig, codegen
+
+
 def bar():
     return foo()
+
+
+def intrinsic_bar():
+    res = intrinsic_foo()
+    return res
 
 
 def test_dpex_overload_from_njit():
@@ -38,4 +66,16 @@ def test_dpex_overload_from_njit():
 
 def test_dpex_overload_from_dpjit():
     bar_dpjit = dpjit(bar)
+    bar_dpjit()
+
+
+def test_dpex_intrinsic_from_njit():
+    bar_njit = njit(intrinsic_bar)
+
+    with pytest.raises(errors.TypingError):
+        bar_njit()
+
+
+def test_dpex_intrinsic_from_dpjit():
+    bar_dpjit = dpjit(intrinsic_bar)
     bar_dpjit()
