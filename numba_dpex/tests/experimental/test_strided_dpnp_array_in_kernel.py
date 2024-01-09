60b9e6bb873dce2c68d2c146cd48a4628f917da0
khaled <akm.khaled.talukder@intel.com>
2024-01-08 21:48:15 -0600

    Test on each subview step

--- numba_dpex/tests/experimental/test_strided_dpnp_array_in_kernel.py
+++ numba_dpex/tests/experimental/test_strided_dpnp_array_in_kernel.py
@@ -59,17 +59,16 @@ def test_multievel_strided_dpnp_array_in_kernel():
     """
     N = 128
     out = dpnp.arange(0, N * 2, dtype=dpnp.int64)
+    v = -3
 
-    K = 7
     b = out
+    n = N
+    K = 7
     for _ in range(K):
         b = b[::2]
-
-    r = Range(int(N / (2 * (K - 1))))
-    v = -3
-    exp_dpex.call_kernel(change_values_1d, r, b, v)
-
-    assert (dpnp.asnumpy(b) == v).all()
+        exp_dpex.call_kernel(change_values_1d, Range(n), b, v)
+        assert (dpnp.asnumpy(b) == v).all()
+        n = int(n / 2)
 
 
 def test_multilevel_2d_strided_dpnp_array_in_kernel():
@@ -79,17 +78,16 @@ def test_multilevel_2d_strided_dpnp_array_in_kernel():
     """
     N = 128
     out, _ = dpnp.mgrid[0 : N * 2, 0 : N * 2]  # noqa: E203
+    v = -3
 
-    K = 7
     b = out
+    n = N
+    K = 7
     for _ in range(K):
         b = b[::2, ::2]
-
-    r = Range(int(N / (2 * (K - 1))), int(N / (2 * (K - 1))))
-    v = -3
-    exp_dpex.call_kernel(change_values_2d, r, b, v)
-
-    assert (dpnp.asnumpy(b) == v).all()
+        exp_dpex.call_kernel(change_values_2d, Range(n, n), b, v)
+        assert (dpnp.asnumpy(b) == v).all()
+        n = int(n / 2)
 
 
 def test_multilevel_3d_strided_dpnp_array_in_kernel():
@@ -99,16 +97,13 @@ def test_multilevel_3d_strided_dpnp_array_in_kernel():
     """
     N = 128
     out, _, _ = dpnp.mgrid[0 : N * 2, 0 : N * 2, 0 : N * 2]  # noqa: E203
+    v = -3
 
-    K = 7
     b = out
+    n = N
+    K = 7
     for _ in range(K):
         b = b[::2, ::2, ::2]
-
-    r = Range(
-        int(N / (2 * (K - 1))), int(N / (2 * (K - 1))), int(N / (2 * (K - 1)))
-    )
-    v = -3
-    exp_dpex.call_kernel(change_values_3d, r, b, v)
-
-    assert (dpnp.asnumpy(b) == v).all()
+        exp_dpex.call_kernel(change_values_3d, Range(n, n, n), b, v)
+        assert (dpnp.asnumpy(b) == v).all()
+        n = int(n / 2)
