b9774d15c828a6193164134efe3f8dcc751651db
Adarsh Yoga <adarsh.yoga@intel.com>
2024-02-05 16:11:03 -0600

    implementation of compare exchange with accompanying test cases

--- numba_dpex/experimental/_kernel_dpcpp_spirv_overloads/spv_fn_generator.py
+++ numba_dpex/experimental/_kernel_dpcpp_spirv_overloads/spv_fn_generator.py
@@ -119,3 +119,59 @@ def get_or_insert_spv_atomic_exchange_fn(context, module, atomic_ref_ty):
     fn.calling_convention = CC_SPIR_FUNC
 
     return fn
+
+
+def get_or_insert_spv_atomic_compare_exchange_fn(
+    context, module, atomic_ref_ty
+):
+    """
+    Gets or inserts a declaration for a __spirv_AtomicCompareExchange call into the
+    specified LLVM IR module.
+    """
+    atomic_ref_dtype = atomic_ref_ty.dtype
+
+    # Spirv spec requires arguments and return type to be of integer types.
+    # That is why the type is changed from float to int
+    # while maintaining the bit-width.
+    # During function call, bitcasting is performed
+    # to adhere to this convention.
+    if atomic_ref_dtype == types.float32:
+        atomic_ref_dtype = types.uint32
+    elif atomic_ref_dtype == types.float64:
+        atomic_ref_dtype = types.uint64
+
+    ptr_type = context.get_value_type(atomic_ref_dtype).as_pointer()
+    ptr_type.addrspace = atomic_ref_ty.address_space
+    atomic_cmpexchg_fn_retty = context.get_value_type(atomic_ref_dtype)
+
+    atomic_cmpexchg_fn_arg_types = [
+        ptr_type,
+        llvmir.IntType(32),
+        llvmir.IntType(32),
+        llvmir.IntType(32),
+        context.get_value_type(atomic_ref_dtype),
+        context.get_value_type(atomic_ref_dtype),
+    ]
+
+    mangled_fn_name = ext_itanium_mangler.mangle_ext(
+        "__spirv_AtomicCompareExchange",
+        [
+            types.CPointer(atomic_ref_dtype, addrspace=ptr_type.addrspace),
+            "__spv.Scope.Flag",
+            "__spv.MemorySemanticsMask.Flag",
+            "__spv.MemorySemanticsMask.Flag",
+            atomic_ref_dtype,
+            atomic_ref_dtype,
+        ],
+    )
+
+    fn = cgutils.get_or_insert_function(
+        module,
+        llvmir.FunctionType(
+            atomic_cmpexchg_fn_retty, atomic_cmpexchg_fn_arg_types
+        ),
+        mangled_fn_name,
+    )
+    fn.calling_convention = CC_SPIR_FUNC
+
+    return fn
