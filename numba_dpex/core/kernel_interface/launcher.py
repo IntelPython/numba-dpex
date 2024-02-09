25addafc6b1b5280a0140c3e48b5db2ce42e6ae5
Diptorup Deb <diptorup.deb@intel.com>
2024-02-08 23:57:15 -0600

    Hook in experimental.call_kernel into dpex.call_kernel

--- numba_dpex/core/kernel_interface/launcher.py
+++ numba_dpex/core/kernel_interface/launcher.py
@@ -19,4 +19,10 @@ def call_kernel(kernel_fn, index_space, *kernel_args) -> None:
         kernel_args : List of objects that are passed to the numba_dpex.kernel
         decorated function.
     """
-    kernel_fn[index_space](*kernel_args)
+    from numba_dpex.experimental import SPIRVKernelDispatcher
+    from numba_dpex.experimental import call_kernel as call_kernel_sycl_mode
+
+    if isinstance(kernel_fn, SPIRVKernelDispatcher):
+        call_kernel_sycl_mode(kernel_fn, index_space, *kernel_args)
+    else:
+        kernel_fn[index_space](*kernel_args)
