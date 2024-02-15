fe54159848911ae1817bb65ed407ef5983098ff8
Diptorup Deb <diptorup.deb@intel.com>
2024-02-15 13:51:45 -0600

    Revert "Move experimental.kernel_iface into top-level module."
    This reverts commit 790e4a05fbd8f04cf456f2bfbd140afce21ac287.

--- numba_dpex/experimental/kernel_iface/__init__.py
+++ numba_dpex/experimental/kernel_iface/__init__.py
@@ -2,8 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""
-The kernel_iface provides a set of Python classes and functions that are
+"""The kernel_iface provides a set of Python classes and functions that are
 analogous to the C++ SYCL API. The kernel_iface API is meant to allow
 prototyping SYCL-like kernels in pure Python before compiling them using
 numba_dpex.kernel.
