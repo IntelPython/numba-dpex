4c8247d9539293f3eb080034928e9ea2617e58ba
Diptorup Deb <diptorup.deb@intel.com>
2024-03-22 01:47:44 -0500

    Get rid of suai_helper

--- numba_dpex/core/utils/__init__.py
+++ numba_dpex/core/utils/__init__.py
@@ -3,11 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .caching_utils import build_key, create_func_hash, strip_usm_metadata
-from .suai_helper import SyclUSMArrayInterface, get_info_from_suai
 
 __all__ = [
-    "get_info_from_suai",
-    "SyclUSMArrayInterface",
     "create_func_hash",
     "strip_usm_metadata",
     "build_key",
