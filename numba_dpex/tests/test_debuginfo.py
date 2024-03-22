63d8495b3cfec298702cfc68f2af197bad7e19ac
Diptorup Deb <diptorup.deb@intel.com>
2024-03-21 20:57:26 -0500

    Port test_debuginfo to new API.

--- numba_dpex/tests/test_debuginfo.py
+++ numba_dpex/tests/test_debuginfo.py
@@ -7,34 +7,23 @@
 import re
 
 import pytest
+from numba.core import types
 
 import numba_dpex as dpex
 from numba_dpex import float32, int32, usm_ndarray
 from numba_dpex.core.descriptor import dpex_kernel_target
+from numba_dpex.core.types.kernel_api.index_space_ids import ItemType
 from numba_dpex.tests._helper import override_config
 
 debug_options = [True, False]
 
 f32arrty = usm_ndarray(ndim=1, dtype=float32, layout="C")
+itemty = ItemType(ndim=1)
 
 
 @pytest.fixture(params=debug_options)
 def debug_option(request):
     return request.param
-
-
-def get_kernel_ir(fn, sig, debug=False):
-    kernel = dpex.core.kernel_interface.spirv_kernel.SpirvKernel(
-        fn, fn.__name__
-    )
-    kernel.compile(
-        args=sig,
-        target_ctx=dpex_kernel_target.target_context,
-        typing_ctx=dpex_kernel_target.typing_context,
-        debug=debug,
-        compile_flags=None,
-    )
-    return kernel.llvm_module
 
 
 def make_check(ir, val_to_search):
@@ -52,11 +41,15 @@ def test_debug_flag_generates_ir_with_debuginfo(debug_option):
     Check debug info is emitting to IR if debug parameter is set to True
     """
 
-    def foo(x):
-        x = 1  # noqa
+    def foo(item, x):
+        i = item.get_id(0)
+        x[i] = 1  # noqa
 
-    sig = (int32,)
-    kernel_ir = get_kernel_ir(foo, sig, debug=debug_option)
+    sig = (itemty, f32arrty)
+    disp = dpex.kernel(sig, debug=debug_option)(foo)
+    kcres = disp.get_compile_result(types.void(itemty, f32arrty))
+    kernel_ir = kcres.library.get_llvm_str()
+
     tag = "!dbg"
 
     if debug_option:
@@ -71,8 +64,8 @@ def test_debug_info_locals_vars_on_no_opt():
     if debug parameter is set to True and optimization is O0
     """
 
-    def foo(var_a, var_b, var_c):
-        i = dpex.get_global_id(0)
+    def foo(item, var_a, var_b, var_c):
+        i = item.get_id(0)
         var_c[i] = var_a[i] + var_b[i]
 
     ir_tags = [
@@ -81,10 +74,14 @@ def test_debug_info_locals_vars_on_no_opt():
         '!DILocalVariable(name: "var_c"',
         '!DILocalVariable(name: "i"',
     ]
-    sig = (f32arrty, f32arrty, f32arrty)
+    sig = (itemty, f32arrty, f32arrty, f32arrty)
 
     with override_config("OPT", 0):
-        kernel_ir = get_kernel_ir(foo, sig, debug=True)
+        disp = dpex.kernel(sig, debug=True)(foo)
+        kcres = disp.get_compile_result(
+            types.void(itemty, f32arrty, f32arrty, f32arrty)
+        )
+        kernel_ir = kcres.library.get_llvm_str()
 
     for tag in ir_tags:
         assert tag in kernel_ir
@@ -96,8 +93,8 @@ def test_debug_kernel_local_vars_in_ir():
     created in kernel
     """
 
-    def foo(arr):
-        index = dpex.get_global_id(0)
+    def foo(item, arr):
+        index = item.get_id(0)
         local_d = 9 * 99 + 5
         arr[index] = local_d + 100
 
@@ -105,8 +102,10 @@ def test_debug_kernel_local_vars_in_ir():
         '!DILocalVariable(name: "index"',
         '!DILocalVariable(name: "local_d"',
     ]
-    sig = (f32arrty,)
-    kernel_ir = get_kernel_ir(foo, sig, debug=True)
+    sig = (itemty, f32arrty)
+    disp = dpex.kernel(sig, debug=True)(foo)
+    kcres = disp.get_compile_result(types.void(itemty, f32arrty))
+    kernel_ir = kcres.library.get_llvm_str()
 
     for tag in ir_tags:
         assert tag in kernel_ir
@@ -117,13 +116,13 @@ def test_debug_flag_generates_ir_with_debuginfo_for_func(debug_option):
     Check debug info is emitting to IR if debug parameter is set to True
     """
 
-    @dpex.func(debug=debug_option)
+    @dpex.device_func(debug=debug_option)
     def func_sum(a, b):
         result = a + b
         return result
 
-    def data_parallel_sum(a, b, c):
-        i = dpex.get_global_id(0)
+    def data_parallel_sum(item, a, b, c):
+        i = item.get_id(0)
         c[i] = func_sum(a[i], b[i])
 
     ir_tags = [
@@ -131,9 +130,12 @@ def test_debug_flag_generates_ir_with_debuginfo_for_func(debug_option):
         r'\!DISubprogram\(name: ".*data_parallel_sum\$?\d*"',
     ]
 
-    sig = (f32arrty, f32arrty, f32arrty)
-
-    kernel_ir = get_kernel_ir(data_parallel_sum, sig, debug=debug_option)
+    sig = (itemty, f32arrty, f32arrty, f32arrty)
+    disp = dpex.kernel(sig, debug=debug_option)(data_parallel_sum)
+    kcres = disp.get_compile_result(
+        types.void(itemty, f32arrty, f32arrty, f32arrty)
+    )
+    kernel_ir = kcres.library.get_llvm_str()
 
     for tag in ir_tags:
         assert debug_option == make_check(kernel_ir, tag)
@@ -144,13 +146,13 @@ def test_env_var_generates_ir_with_debuginfo_for_func(debug_option):
     Check debug info is emitting to IR if NUMBA_DPEX_DEBUGINFO is set to 1
     """
 
-    @dpex.func
+    @dpex.device_func(debug=debug_option)
     def func_sum(a, b):
         result = a + b
         return result
 
-    def data_parallel_sum(a, b, c):
-        i = dpex.get_global_id(0)
+    def data_parallel_sum(item, a, b, c):
+        i = item.get_id(0)
         c[i] = func_sum(a[i], b[i])
 
     ir_tags = [
@@ -158,43 +160,52 @@ def test_env_var_generates_ir_with_debuginfo_for_func(debug_option):
         r'\!DISubprogram\(name: ".*data_parallel_sum\$\d*"',
     ]
 
-    sig = (f32arrty, f32arrty, f32arrty)
+    sig = (itemty, f32arrty, f32arrty, f32arrty)
 
     with override_config("DEBUGINFO_DEFAULT", int(debug_option)):
-        kernel_ir = get_kernel_ir(data_parallel_sum, sig)
+        disp = dpex.kernel(sig, debug=debug_option, inline_threshold=0)(
+            data_parallel_sum
+        )
+        kcres = disp.get_compile_result(
+            types.void(itemty, f32arrty, f32arrty, f32arrty)
+        )
+        kernel_ir = kcres.library.get_llvm_str()
 
     for tag in ir_tags:
         assert debug_option == make_check(kernel_ir, tag)
 
 
 def test_debuginfo_DISubprogram_linkageName():
-    def func(a, b):
-        i = dpex.get_global_id(0)
+    def foo(item, a, b):
+        i = item.get_id(0)
         b[i] = a[i]
 
     ir_tags = [
-        r'\!DISubprogram\(.*linkageName: ".*func.*"',
+        r'\!DISubprogram\(.*linkageName: ".*foo.*"',
     ]
 
-    sig = (f32arrty, f32arrty)
-    kernel_ir = get_kernel_ir(func, sig, debug=True)
+    sig = (itemty, f32arrty, f32arrty)
+    disp = dpex.kernel(sig, debug=debug_option)(foo)
+    kcres = disp.get_compile_result(types.void(itemty, f32arrty, f32arrty))
+    kernel_ir = kcres.library.get_llvm_str()
 
     for tag in ir_tags:
         assert make_check(kernel_ir, tag)
 
 
 def test_debuginfo_DICompileUnit_language_and_producer():
-    def func(a, b):
-        i = dpex.get_global_id(0)
+    def foo(item, a, b):
+        i = item.get_id(0)
         b[i] = a[i]
 
     ir_tags = [
         r"\!DICompileUnit\(language: DW_LANG_C_plus_plus,",
     ]
 
-    sig = (f32arrty, f32arrty)
-
-    kernel_ir = get_kernel_ir(func, sig, debug=True)
+    sig = (itemty, f32arrty, f32arrty)
+    disp = dpex.kernel(sig, debug=debug_option)(foo)
+    kcres = disp.get_compile_result(types.void(itemty, f32arrty, f32arrty))
+    kernel_ir = kcres.library.get_llvm_str()
 
     for tag in ir_tags:
         assert make_check(kernel_ir, tag)
