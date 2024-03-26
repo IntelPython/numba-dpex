c7986c3c3546b289f5a9f24dd435e90373c904e5
Diptorup Deb <diptorup.deb@intel.com>
2024-03-26 14:35:30 -0500

    Merge experimental.typeof into core.typeof

--- numba_dpex/experimental/__init__.py
+++ numba_dpex/experimental/__init__.py
@@ -5,14 +5,3 @@
 """Contains experimental features that are meant as engineering preview and not
 yet production ready.
 """
-
-
-# Temporary so that Range and NdRange work in experimental call_kernel
-from numba_dpex.core.boxing import *
-from numba_dpex.kernel_api_impl.spirv.dispatcher import SPIRVKernelDispatcher
-
-from . import typeof
-
-__all__ = [
-    "SPIRVKernelDispatcher",
-]
