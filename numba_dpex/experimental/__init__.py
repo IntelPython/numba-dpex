77295480d9dd3b5dbef579eb992805483ea28c4f
Diptorup Deb <diptorup.deb@intel.com>
2024-03-26 14:44:21 -0500

    Migrate experimental.testing and delete the experimental module.

--- numba_dpex/experimental/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2023 - 2024 Intel Corporation
-#
-# SPDX-License-Identifier: Apache-2.0
-
-"""Contains experimental features that are meant as engineering preview and not
-yet production ready.
-"""
